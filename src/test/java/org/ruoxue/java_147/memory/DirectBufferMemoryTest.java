808d5c473bd9c95203de02b1fcc699cbba02e912
ruoxue <ruoxue777@gmail.com>
2022-12-26 15:35:45 +0800

    update

--- src/test/java/org/ruoxue/java_147/memory/DirectBufferMemoryTest.java
+++ src/test/java/org/ruoxue/java_147/memory/DirectBufferMemoryTest.java
@@ -1,7 +1,5 @@
 package org.ruoxue.java_147.memory;
 
-import static org.junit.Assert.*;
-
 import java.nio.ByteBuffer;
 
 import org.junit.Test;
@@ -10,16 +8,16 @@ public class DirectBufferMemoryTest {
 
 	@Test
 	public void allocate() {
-		System.out.println("freeMemory: "+Runtime.getRuntime().freeMemory() / 1024 / 1024+ " MB");
+		System.out.println("freeMemory: " + Runtime.getRuntime().freeMemory() / 1024 / 1024 + " MB");
 		ByteBuffer buffer = ByteBuffer.allocate(Integer.MAX_VALUE - 2);
-		System.out.println("freeMemory: "+Runtime.getRuntime().freeMemory() / 1024 / 1024+ " MB");
+		System.out.println("freeMemory: " + Runtime.getRuntime().freeMemory() / 1024 / 1024 + " MB");
 	}
 
 	@Test
 	public void allocateDirect() {
-		System.out.println("freeMemory: "+Runtime.getRuntime().freeMemory() / 1024 / 1024+ " MB");
+		System.out.println("freeMemory: " + Runtime.getRuntime().freeMemory() / 1024 / 1024 + " MB");
 		ByteBuffer buffer = ByteBuffer.allocateDirect(Integer.MAX_VALUE);
-		System.out.println("freeMemory: "+Runtime.getRuntime().freeMemory() / 1024 / 1024+ " MB");
+		System.out.println("freeMemory: " + Runtime.getRuntime().freeMemory() / 1024 / 1024 + " MB");
 	}
 
 }
