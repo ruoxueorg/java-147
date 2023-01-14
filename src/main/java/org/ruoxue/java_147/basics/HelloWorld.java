090800e7009850787d577757fe4d1d0f7cd6f30a
ruoxue <ruoxue777@gmail.com>
2023-01-14 21:43:48 +0800

    update Hello World

--- src/main/java/org/ruoxue/java_147/basics/HelloWorld.java
+++ src/main/java/org/ruoxue/java_147/basics/HelloWorld.java
@@ -3,6 +3,6 @@ package org.ruoxue.java_147.basics;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		System.out.println("Hello World");
 	}
 }
