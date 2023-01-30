4c75c14cd575f7ead9450e3c664a9eb57f3963c4
ruoxue <ruoxue777@gmail.com>
2023-01-30 22:32:23 +0800

    update test

--- src/test/java/org/ruoxue/java_147/queue/LinkedListMethodsTest.java
+++ src/test/java/org/ruoxue/java_147/queue/LinkedListMethodsTest.java
@@ -2,8 +2,146 @@ package org.ruoxue.java_147.queue;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class LinkedListMethodsTest {
+
+	public LinkedListMethodsTest() {
+
+	}
+
+	@Test
+	public void add() {
+		int expectedSize = 3;
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		System.out.println(list);
+		assertEquals(expectedSize, list.size());
+	}
+
+	@Test
+	public void addByIndex() {
+		int expectedSize = 4;
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		list.add(2, "Durian");
+		System.out.println(list);
+		assertEquals(expectedSize, list.size());
+	}
+
+	@Test
+	public void addAll() {
+		int expectedSize = 6;
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+
+		List<String> list2 = new LinkedList<String>();
+		list2.add("Durian");
+		list2.add("Guava");
+		list2.add("Pitaya");
+
+		list.addAll(list2);
+		System.out.println(list);
+		assertEquals(expectedSize, list.size());
+	}
+
+	@Test
+	public void get() {
+		String expected = "Strawberry";
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		String value = list.get(1);
+		System.out.println(value);
+		assertEquals(expected, value);
+	}
+
+	@Test
+	public void set() {
+		String expected = "Durian";
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		System.out.println(list);
+
+		list.set(0, "Durian");
+		System.out.println(list);
+		assertEquals(expected, list.get(0));
+	}
+
+	@Test
+	public void remove() {
+		int expectedSize = 2;
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		list.remove(0);
+		System.out.println(list);
+		assertEquals(expectedSize, list.size());
+	}
+
+	@Test
+	public void removeAll() {
+		int expectedSize = 1;
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+
+		List<String> list2 = new LinkedList<String>();
+		list2.add("Papaya");
+		list2.add("Strawberry");
+		list2.add("Pitaya");
+		list.removeAll(list2);
+		System.out.println(list);
+		assertEquals(expectedSize, list.size());
+	}
+
+	@Test
+	public void clear() {
+		int expectedSize = 0;
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		list.clear();
+		System.out.println(list);
+		assertEquals(expectedSize, list.size());
+	}
+
+	@Test
+	public void size() {
+		int expectedSize = 3;
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		System.out.println(list.size());
+		assertEquals(expectedSize, list.size());
+	}
+
+	@Test
+	public void isEmpty() {
+		List<String> list = new LinkedList<String>();
+		System.out.println(list.isEmpty());
+		assertTrue(list.isEmpty());
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		System.out.println(list.isEmpty());
+		assertFalse(list.isEmpty());
+	}
 
 }
--- src/test/java/org/ruoxue/java_147/queue/LinkedListWithExamplesTest.java
+++ src/test/java/org/ruoxue/java_147/queue/LinkedListWithExamplesTest.java
@@ -2,8 +2,147 @@ package org.ruoxue.java_147.queue;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Spliterator;
+
 import org.junit.Test;
 
 public class LinkedListWithExamplesTest {
+
+	public LinkedListWithExamplesTest() {
+
+	}
+
+	@Test
+	public void loop() {
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+	}
+
+	@Test
+	public void forEach() {
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		list.forEach(e -> System.out.println(e));
+	}
+
+	@Test
+	public void forEachRemaining() {
+		List<String> list = new LinkedList<>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		Iterator<String> it = list.iterator();
+		int i = 0;
+		while (it.hasNext()) {
+			System.out.println(it.next());
+			if (i == 1) {
+				break;
+			}
+			i++;
+		}
+		System.out.println("----------");
+		it.forEachRemaining(e -> {
+			System.out.println(e);
+		});
+	}
+
+	@Test
+	public void iterator() {
+		List<String> list = new LinkedList<>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		Iterator<String> it = list.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+
+	@Test
+	public void listIterator() {
+		List<String> list = new LinkedList<>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		ListIterator<String> it = list.listIterator(list.size());
+		while (it.hasPrevious()) {
+			System.out.println(it.previous());
+		}
+	}
+
+	@Test
+	public void spliterator() {
+		List<String> list = new LinkedList<>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		Spliterator<String> sit = list.spliterator();
+		sit.tryAdvance(e -> System.out.println(e));
+		System.out.println("----------");
+		sit.forEachRemaining(e -> System.out.println(e));
+
+		System.out.println("----------");
+		sit = list.spliterator();
+		while (sit.tryAdvance(e -> System.out.println(e))) {
+		}
+	}
+
+	@Test
+	public void trySplit() {
+		List<String> list = new LinkedList<>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+		Spliterator<String> sit = list.spliterator();
+		Spliterator<String> sit2 = sit.trySplit();
+		System.out.println(sit.getExactSizeIfKnown());
+		sit.forEachRemaining(e -> System.out.println(e));
+
+		System.out.println("----------");
+		System.out.println(sit2.getExactSizeIfKnown());
+		sit2.forEachRemaining(e -> System.out.println(e));
+	}
+
+	@Test
+	public void toArray() {
+		int expectedSize = 3;
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+
+		String[] array = new String[list.size()];
+		list.toArray(array);
+		for (String e : array) {
+			System.out.println(e);
+		}
+		assertEquals(expectedSize, array.length);
+	}
+
+	@Test
+	public void toArrayByStream() {
+		int expectedSize = 3;
+		List<String> list = new LinkedList<String>();
+		list.add("Papaya");
+		list.add("Strawberry");
+		list.add("Watermelon");
+
+		String[] array = list.stream().toArray(String[]::new);
+		for (String e : array) {
+			System.out.println(e);
+		}
+		assertEquals(expectedSize, array.length);
+	}
 
 }
