ad4e7ca661d955f491ad70ad88647a7de746111e
ruoxue <ruoxue777@gmail.com>
2023-01-31 01:02:27 +0800

    fix bug

--- src/test/java/org/ruoxue/java_147/queue/LinkedListMethodsTest.java
+++ src/test/java/org/ruoxue/java_147/queue/LinkedListMethodsTest.java
@@ -1,6 +1,8 @@
 package org.ruoxue.java_147.queue;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.LinkedList;
 import java.util.List;
