114b2b95cd6010d626e2d15cbeef45ac1403d358
ruoxue <ruoxue777@gmail.com>
2023-02-08 17:53:15 +0800

    add ThreadJoinTest

---
+++ src/test/java/org/ruoxue/java_147/multithreading/ThreadJoinTest.java
@@ -0,0 +1,14 @@
+package org.ruoxue.java_147.multithreading;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ThreadJoinTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
