403a23a3b9e3436e11d76323c9da458d31ebcd58
ruoxue <ruoxue777@gmail.com>
2023-02-09 01:24:58 +0800

    add test

---
+++ src/test/java/org/ruoxue/java_147/multithreading/WaitNotifyTest.java
@@ -0,0 +1,14 @@
+package org.ruoxue.java_147.multithreading;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WaitNotifyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
