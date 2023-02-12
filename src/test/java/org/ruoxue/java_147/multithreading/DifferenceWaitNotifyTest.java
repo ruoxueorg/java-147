a676179850cd5f27f16fe0e834d500c0c61a2939
ruoxue <ruoxue777@gmail.com>
2023-02-12 18:53:45 +0800

    add waitInterrupt

--- src/test/java/org/ruoxue/java_147/multithreading/DifferenceWaitNotifyTest.java
+++ src/test/java/org/ruoxue/java_147/multithreading/DifferenceWaitNotifyTest.java
@@ -3,14 +3,12 @@ package org.ruoxue.java_147.multithreading;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
 
 import org.junit.Test;
 
 public class DifferenceWaitNotifyTest {
+
 	protected class Worker {
 
 		private volatile boolean done = false;
@@ -20,7 +18,7 @@ public class DifferenceWaitNotifyTest {
 
 		public synchronized void put() throws InterruptedException {
 			try {
-				TimeUnit.SECONDS.sleep(5);
+				TimeUnit.SECONDS.sleep(3);
 				System.out.println("T[" + Thread.currentThread().getId() + "] put finished");
 			} finally {
 				done = true;
@@ -29,7 +27,6 @@ public class DifferenceWaitNotifyTest {
 		}
 
 		public synchronized boolean take() throws InterruptedException {
-			System.out.println("T[" + Thread.currentThread().getId() + "] take waiting " + done);
 			while (!done) {
 				System.out.println("T[" + Thread.currentThread().getId() + "] take waiting");
 				wait();
@@ -64,15 +61,84 @@ public class DifferenceWaitNotifyTest {
 			}
 		});
 
-		threadA.start();
 		threadB.start();
+		threadA.start();
 
 		try {
-			TimeUnit.SECONDS.sleep(10);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			threadA.join();
+			threadB.join();
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	protected class InterruptWorker {
+
+		private volatile boolean done = false;
+
+		public InterruptWorker() {
 		}
 
+		public synchronized void put() throws InterruptedException {
+			try {
+				TimeUnit.SECONDS.sleep(3);
+				System.out.println("T[" + Thread.currentThread().getId() + "] put finished");
+			} finally {
+				done = true;
+			}
+		}
+
+		public synchronized boolean take() throws InterruptedException {
+			while (!done) {
+				System.out.println("T[" + Thread.currentThread().getId() + "] take waiting");
+				wait();
+			}
+			return done;
+		}
 	}
+
+	@Test
+	public void waitInterrupt() {
+		InterruptWorker worker = new InterruptWorker();
+		Thread threadA = new Thread(() -> {
+			String id = "A";
+			try {
+				System.out.println("T[" + Thread.currentThread().getId() + "] worker: " + id + " ready");
+				worker.put();
+				System.out.println("T[" + Thread.currentThread().getId() + "] worker: " + id + " finished");
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+		});
+
+		Thread threadB = new Thread(() -> {
+			String id = "B";
+			try {
+				System.out.println("T[" + Thread.currentThread().getId() + "] worker: " + id + " ready");
+				boolean done = worker.take();
+				System.out.println(
+						"T[" + Thread.currentThread().getId() + "] worker: " + id + " finished, result: " + done);
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+		});
+
+		threadB.start();
+		threadA.start();
+
+		try {
+			threadA.join();
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+
+		long startTime = System.currentTimeMillis();
+		while (true) {
+			if (System.currentTimeMillis() - startTime > 3000) {
+				threadB.interrupt();
+				break;
+			}
+		}
+	}
+
 }
