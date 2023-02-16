751da64a90204b588cffdd6173e464ed68641cca
ruoxue <ruoxue777@gmail.com>
2023-02-16 18:00:29 +0800

    add test

---
+++ src/test/java/org/ruoxue/java_147/multithreading/ExecutorServiceMethodsTest.java
@@ -0,0 +1,23 @@
+package org.ruoxue.java_147.multithreading;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+public class ExecutorServiceMethodsTest {
+
+	@Test
+	public void execute() {
+		int corePoolSize = Runtime.getRuntime().availableProcessors();
+		ExecutorService executorService = new ThreadPoolExecutor(corePoolSize, corePoolSize * 50, 60L, TimeUnit.SECONDS,
+				new SynchronousQueue<Runnable>());
+
+	}
+
+}
---
+++ src/test/java/org/ruoxue/java_147/multithreading/ExecutorServiceWithExamplesTest.java
@@ -0,0 +1,14 @@
+package org.ruoxue.java_147.multithreading;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExecutorServiceWithExamplesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
