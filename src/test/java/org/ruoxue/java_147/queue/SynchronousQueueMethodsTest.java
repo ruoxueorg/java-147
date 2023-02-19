96d4c3d3873bc220418c826f678f2682b06ceae8
ruoxue <ruoxue777@gmail.com>
2023-02-19 12:07:19 +0800

    add SynchronousQueueMethodsTest

---
+++ src/test/java/org/ruoxue/java_147/queue/SynchronousQueueMethodsTest.java
@@ -0,0 +1,13 @@
+package org.ruoxue.java_147.queue;
+
+import static org.junit.Assert.*;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.BlockingQueue;
+
+import org.junit.Test;
+
+public class SynchronousQueueMethodsTest {
+
+}
