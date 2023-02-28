2a9795b0e76313fb4ee70a7a2182c1cca2a19216
ruoxue <ruoxue777@gmail.com>
2023-02-28 20:10:17 +0800

    add test

---
+++ src/test/java/org/ruoxue/java_147/charz/CharToStringTest.java
@@ -0,0 +1,14 @@
+package org.ruoxue.java_147.charz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CharToStringTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/org/ruoxue/java_147/enumz/EnumToStringTest.java
@@ -0,0 +1,14 @@
+package org.ruoxue.java_147.enumz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EnumToStringTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
