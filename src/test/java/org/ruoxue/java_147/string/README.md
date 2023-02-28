773e79ea0db3ede49c363da2a4efbe3bb47bc9bb
ruoxue <ruoxue777@gmail.com>
2023-02-28 19:39:25 +0800

    add StringToInputStreamTest

--- src/test/java/org/ruoxue/java_147/string/README.md
+++ src/test/java/org/ruoxue/java_147/string/README.md
@@ -4,3 +4,4 @@
 1. [Java String format Method](https://www.ruoxue.org/java-147-java-string-format-method/)
 2. [Java Convert String to Integer Array](https://www.ruoxue.org/java-147-java-convert-string-to-integer-array/)
 3. [Java Convert String to String Array](https://www.ruoxue.org/java-147-java-convert-string-to-string-array/)
+4. [Java Convert String to InputStream](https://www.ruoxue.org/java-147-java-convert-string-to-inputstream/)
---
+++ src/test/java/org/ruoxue/java_147/string/StringToInputStreamTest.java
@@ -0,0 +1,77 @@
+package org.ruoxue.java_147.string;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import com.google.common.io.CharSource;
+
+public class StringToInputStreamTest {
+
+	@Test
+	public void byteArrayInputStream() {
+		String value = "java147,springboot168,junit151,bash460,it484";
+		InputStream is = new ByteArrayInputStream(value.getBytes(StandardCharsets.UTF_8));
+		System.out.println(is);
+
+		StringBuilder builder = new StringBuilder();
+		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+			String str;
+			while ((str = bufferedReader.readLine()) != null) {
+				builder.append(str);
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		String result = builder.toString();
+		System.out.println(result);
+		assertEquals(value, result);
+	}
+
+	@Test
+	public void ioUtils() {
+		String value = "java147,springboot168,junit151,bash460,it484";
+		InputStream is = IOUtils.toInputStream(value, StandardCharsets.UTF_8);
+		System.out.println(is);
+
+		String result = null;
+		try {
+			result = IOUtils.toString(is, StandardCharsets.UTF_8);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println(result);
+		assertEquals(value, result);
+	}
+
+	@Test
+	public void charSource() {
+		String value = "java147,springboot168,junit151,bash460,it484";
+		InputStream is = null;
+		try {
+			is = CharSource.wrap(value).asByteSource(StandardCharsets.UTF_8).openStream();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println(is);
+
+		StringBuilder builder = new StringBuilder();
+		try (Scanner scanner = new Scanner(is, StandardCharsets.UTF_8.toString())) {
+			while (scanner.hasNext()) {
+				builder.append(scanner.nextLine());
+			}
+		}
+		String result = builder.toString();
+		System.out.println(result);
+		assertEquals(value, result);
+	}
+}
