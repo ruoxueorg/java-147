04bf4d37e01fe2d9efa09aaf57a8699d075ff3b9
ruoxue <ruoxue777@gmail.com>
2023-03-03 17:54:53 +0800

    add

---
+++ src/test/java/org/ruoxue/java_147/string/StringToBooleanTest.java
@@ -0,0 +1,82 @@
+package org.ruoxue.java_147.string;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringToBooleanTest {
+
+	@Test
+	public void parseBoolean() {
+		String value = "true";
+		boolean result = Boolean.parseBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+
+		value = "True";
+		result = Boolean.parseBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+
+		value = "TRUE";
+		result = Boolean.parseBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+
+		value = "ok";
+		result = Boolean.parseBoolean(value);
+		System.out.println(result);
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void valueOf() {
+		String value = "true";
+		Boolean result = Boolean.valueOf(value);
+		System.out.println(result);
+		assertEquals(true, result.booleanValue());
+
+		value = "True";
+		result = Boolean.valueOf(value);
+		System.out.println(result);
+		assertEquals(true, result.booleanValue());
+
+		value = "TRUE";
+		result = Boolean.valueOf(value);
+		System.out.println(result);
+		assertEquals(true, result.booleanValue());
+
+		value = "ok";
+		result = Boolean.valueOf(value);
+		System.out.println(result);
+		assertEquals(false, result.booleanValue());
+	}
+
+	@Test
+	public void getBoolean() {
+		String value = "TEST_MODE";
+		boolean result = Boolean.getBoolean(value);
+		System.out.println(result);
+		assertEquals(false, result);
+
+		System.setProperty("TEST_MODE", "true");
+		result = Boolean.getBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+
+		System.setProperty("TEST_MODE", "True");
+		result = Boolean.getBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+
+		System.setProperty("TEST_MODE", "TRUE");
+		result = Boolean.getBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+
+		System.setProperty("TEST_MODE", "ok");
+		result = Boolean.getBoolean(value);
+		System.out.println(result);
+		assertEquals(false, result);
+	}
+}
---
+++ src/test/java/org/ruoxue/java_147/string/StringToBooleanWithExamplesTest.java
@@ -0,0 +1,71 @@
+package org.ruoxue.java_147.string;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.lang3.BooleanUtils;
+import org.junit.Test;
+
+public class StringToBooleanWithExamplesTest {
+
+	@Test
+	public void constructor() {
+		String value = "true";
+		boolean result = new Boolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+
+		value = "True";
+		result = new Boolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+
+		value = "TRUE";
+		result = new Boolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+
+		value = "ok";
+		result = new Boolean(value);
+		System.out.println(result);
+		assertEquals(false, result);
+	}
+
+
+	@Test
+	public void toBoolean_BooleanUtils() {
+		String value = "true";
+		boolean result = BooleanUtils.toBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+
+		value = "True";
+		result = BooleanUtils.toBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+		
+		value = "TRUE";
+		result = BooleanUtils.toBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+		
+		value = "ok";
+		result = BooleanUtils.toBoolean(value);
+		System.out.println(result);
+		assertEquals(false, result);
+		
+		value = "y";
+		result = BooleanUtils.toBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+		
+		value = "yes";
+		result = BooleanUtils.toBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+		
+		value = "on";
+		result = BooleanUtils.toBoolean(value);
+		System.out.println(result);
+		assertEquals(true, result);
+	}
+}
