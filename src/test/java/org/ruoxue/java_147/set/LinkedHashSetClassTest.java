4fa7c1be4e123e1d32cee723d041aeca18a10162
ruoxue <ruoxue777@gmail.com>
2023-03-06 16:05:51 +0800

    add test

---
+++ src/test/java/org/ruoxue/java_147/set/LinkedHashSetClassTest.java
@@ -0,0 +1,89 @@
+package org.ruoxue.java_147.set;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+public class LinkedHashSetClassTest {
+
+	@Test
+	public void contains() {
+		Set<String> set = new HashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+
+		boolean contains = set.contains("Longan");
+		System.out.println(contains);
+		assertTrue(contains);
+
+		contains = set.contains("Grape");
+		System.out.println(contains);
+		assertFalse(contains);
+	}
+
+	@Test
+	public void containsAll() {
+		Set<String> set = new HashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+
+		Set<String> set2 = new HashSet<String>();
+		set2.add("Longan");
+		set2.add("Tomato");
+
+		boolean contains = set.containsAll(set2);
+		System.out.println(contains);
+		assertTrue(contains);
+
+		contains = set2.containsAll(set);
+		System.out.println(contains);
+		assertFalse(contains);
+	}
+
+	@Test
+	public void stream() {
+		int expectedSize = 1;
+		Set<String> set = new HashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+		set = set.stream().filter(e -> e.length() < 6).collect(Collectors.toSet());
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void parallelStream() {
+		Set<String> set = new HashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+		set.parallelStream().forEach(System.out::println);
+		System.out.println("----------");
+		set.parallelStream().forEachOrdered(System.out::println);
+	}
+
+	@Test
+	public void retainAll() {
+		int expectedSize = 1;
+		Set<String> set = new HashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+
+		Set<String> set2 = new HashSet<String>();
+		set2.add("Longan");
+		set2.add("Lemon");
+		set2.add("Mango");
+
+		set.retainAll(set2);
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+}
---
+++ src/test/java/org/ruoxue/java_147/set/LinkedHashSetMethodsTest.java
@@ -0,0 +1,104 @@
+package org.ruoxue.java_147.set;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class LinkedHashSetMethodsTest {
+
+	@Test
+	public void add() {
+		int expectedSize = 3;
+		Set<String> set = new LinkedHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void addAll() {
+		int expectedSize = 6;
+		Set<String> set = new LinkedHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+
+		Set<String> set2 = new LinkedHashSet<String>();
+		set2.add("Grape");
+		set2.add("Lemon");
+		set2.add("Mango");
+
+		set.addAll(set2);
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void remove() {
+		int expectedSize = 2;
+		Set<String> set = new LinkedHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+		set.remove("Longan");
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void removeAll() {
+		int expectedSize = 1;
+		Set<String> set = new LinkedHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+
+		Set<String> set2 = new LinkedHashSet<String>();
+		set2.add("Longan");
+		set2.add("Tomato");
+		set2.add("Mango");
+		set.removeAll(set2);
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void clear() {
+		int expectedSize = 0;
+		Set<String> set = new LinkedHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+		set.clear();
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void size() {
+		int expectedSize = 3;
+		Set<String> set = new LinkedHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+		System.out.println(set.size());
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void isEmpty() {
+		Set<String> set = new LinkedHashSet<String>();
+		System.out.println(set.isEmpty());
+		assertTrue(set.isEmpty());
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+		System.out.println(set.isEmpty());
+		assertFalse(set.isEmpty());
+	}
+}
