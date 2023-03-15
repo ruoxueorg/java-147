8a8b705af42102a97bccedee8d9c3818379a8374
ruoxue <ruoxue777@gmail.com>
2023-03-15 14:59:06 +0800

    update

--- src/test/java/org/ruoxue/java_147/multithreading/ScheduledExecutorServiceInterfaceTest.java
+++ src/test/java/org/ruoxue/java_147/multithreading/ScheduledExecutorServiceInterfaceTest.java
@@ -18,22 +18,18 @@ public class ScheduledExecutorServiceInterfaceTest {
 		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		System.out.println(String.format("%s T[%d] init", df.format(new Date()), Thread.currentThread().getId()));
 		long start = System.currentTimeMillis();
-		ScheduledFuture<?> futureA = executorService.scheduleAtFixedRate(() -> {
+		ScheduledFuture<?> future = executorService.scheduleAtFixedRate(() -> {
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 			try {
-				System.out.println(String.format("%s T[%d] %d task: A ready", sdf.format(new Date()),
+				System.out.println(String.format("%s T[%d] %d task ready", sdf.format(new Date()),
 						Thread.currentThread().getId(), System.currentTimeMillis() - start));
 				for (int i = 0; i < 100000; i++) {
 					byte[] bytes = new byte[1024 * 1000];
-					if (Thread.interrupted()) {
-						throw new InterruptedException();
-					}
 				}
-				System.out.println(String.format("%s T[%d] %d task: A finished", sdf.format(new Date()),
+				System.out.println(String.format("%s T[%d] %d task finished", sdf.format(new Date()),
 						Thread.currentThread().getId(), System.currentTimeMillis() - start));
-			} catch (InterruptedException ex) {
+			} catch (Exception ex) {
 				ex.printStackTrace();
-				Thread.currentThread().interrupt();
 			}
 		}, 1_000, 1_000, TimeUnit.MILLISECONDS);
 
@@ -43,14 +39,14 @@ public class ScheduledExecutorServiceInterfaceTest {
 			ex.printStackTrace();
 		}
 
-		if (false == futureA.isDone()) {
-			boolean cancel = futureA.cancel(false);
-			System.out.println(String.format("%s T[%d] %d task: A, cancel: %b, done: %b", df.format(new Date()),
-					Thread.currentThread().getId(), System.currentTimeMillis() - start, cancel,futureA.isDone()));
+		if (false == future.isDone()) {
+			boolean cancel = future.cancel(false);
+			System.out.println(String.format("%s T[%d] %d task cancel: %b, done: %b", df.format(new Date()),
+					Thread.currentThread().getId(), System.currentTimeMillis() - start, cancel, future.isDone()));
 		}
 
 		try {
-			executorService.awaitTermination(30, TimeUnit.SECONDS);
+			executorService.awaitTermination(20, TimeUnit.SECONDS);
 		} catch (InterruptedException ex) {
 			ex.printStackTrace();
 		}
@@ -63,10 +59,10 @@ public class ScheduledExecutorServiceInterfaceTest {
 		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		System.out.println(String.format("%s T[%d] init", df.format(new Date()), Thread.currentThread().getId()));
 		long start = System.currentTimeMillis();
-		ScheduledFuture<?> futureA = executorService.scheduleAtFixedRate(() -> {
+		ScheduledFuture<?> future = executorService.scheduleAtFixedRate(() -> {
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 			try {
-				System.out.println(String.format("%s T[%d] %d task: A ready", sdf.format(new Date()),
+				System.out.println(String.format("%s T[%d] %d task ready", sdf.format(new Date()),
 						Thread.currentThread().getId(), System.currentTimeMillis() - start));
 				for (int i = 0; i < 100000; i++) {
 					byte[] bytes = new byte[1024 * 1000];
@@ -74,7 +70,7 @@ public class ScheduledExecutorServiceInterfaceTest {
 						throw new InterruptedException();
 					}
 				}
-				System.out.println(String.format("%s T[%d] %d task: A finished", sdf.format(new Date()),
+				System.out.println(String.format("%s T[%d] %d task finished", sdf.format(new Date()),
 						Thread.currentThread().getId(), System.currentTimeMillis() - start));
 			} catch (InterruptedException ex) {
 				ex.printStackTrace();
@@ -88,14 +84,14 @@ public class ScheduledExecutorServiceInterfaceTest {
 			ex.printStackTrace();
 		}
 
-		if (false == futureA.isDone()) {
-			boolean cancel = futureA.cancel(true);
-			System.out.println(String.format("%s T[%d] %d task: A, cancel: %b", df.format(new Date()),
+		if (false == future.isDone()) {
+			boolean cancel = future.cancel(true);
+			System.out.println(String.format("%s T[%d] %d task cancel: %b", df.format(new Date()),
 					Thread.currentThread().getId(), System.currentTimeMillis() - start, cancel));
 		}
 
 		try {
-			executorService.awaitTermination(10, TimeUnit.SECONDS);
+			executorService.awaitTermination(20, TimeUnit.SECONDS);
 		} catch (InterruptedException ex) {
 			ex.printStackTrace();
 		}
--- src/test/java/org/ruoxue/java_147/multithreading/ScheduledThreadPoolExecutorClassTest.java
+++ src/test/java/org/ruoxue/java_147/multithreading/ScheduledThreadPoolExecutorClassTest.java
@@ -1,14 +1,231 @@
 package org.ruoxue.java_147.multithreading;
 
-import static org.junit.Assert.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Test;
 
 public class ScheduledThreadPoolExecutorClassTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	protected class FalseWorker implements Runnable {
+
+		private AtomicInteger counter;
+		private long start;
+
+		public FalseWorker(AtomicInteger counter, long start) {
+			this.counter = counter;
+			this.start = start;
+		}
+
+		@Override
+		public void run() {
+			try {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+				System.out.println(String.format("%s T[%d] %d worker ready", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start));
+				counter.getAndIncrement();
+				System.out.println(String.format("%s T[%d] %d worker finished, counter: %d", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start, counter.get()));
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 
+	@Test
+	public void cancelFalse() {
+		int poolSize = 1;
+		ScheduledExecutorService executorService = Executors.newScheduledThreadPool(poolSize);
+		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		System.out.println(String.format("%s T[%d] init", df.format(new Date()), Thread.currentThread().getId()));
+		long start = System.currentTimeMillis();
+		AtomicInteger counter = new AtomicInteger();
+		ScheduledFuture<?> future = executorService.scheduleAtFixedRate(new FalseWorker(counter, start), 1_000, 1_000,
+				TimeUnit.MILLISECONDS);
+
+		while (true) {
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+			if (counter.get() == 3) {
+				if (false == future.isDone()) {
+					boolean cancel = future.cancel(false);
+					System.out.println(String.format("%s T[%d] %d worker cancel: %b, done: %b", df.format(new Date()),
+							Thread.currentThread().getId(), System.currentTimeMillis() - start, cancel,
+							future.isDone()));
+				}
+				break;
+			}
+		}
+
+		try {
+			executorService.awaitTermination(10, TimeUnit.SECONDS);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	protected class TrueWorker implements Runnable {
+
+		private AtomicInteger counter;
+		private long start;
+
+		public TrueWorker(AtomicInteger counter, long start) {
+			this.counter = counter;
+			this.start = start;
+		}
+
+		@Override
+		public void run() {
+			try {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+				System.out.println(String.format("%s T[%d] %d worker ready", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start));
+				counter.getAndIncrement();
+				TimeUnit.SECONDS.sleep(1);
+				System.out.println(String.format("%s T[%d] %d worker finished, counter: %d", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start, counter.get()));
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
+
+	@Test
+	public void cancelTrue() {
+		int poolSize = 1;
+		ScheduledExecutorService executorService = Executors.newScheduledThreadPool(poolSize);
+		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		System.out.println(String.format("%s T[%d] init", df.format(new Date()), Thread.currentThread().getId()));
+		long start = System.currentTimeMillis();
+		AtomicInteger counter = new AtomicInteger();
+		ScheduledFuture<?> future = executorService.scheduleAtFixedRate(new TrueWorker(counter, start), 1_000, 1_000,
+				TimeUnit.MILLISECONDS);
+
+		try {
+			TimeUnit.SECONDS.sleep(4);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+
+		if (false == future.isDone()) {
+			boolean cancel = future.cancel(true);
+			System.out.println(String.format("%s T[%d] %d worker cancel: %b, done: %b", df.format(new Date()),
+					Thread.currentThread().getId(), System.currentTimeMillis() - start, cancel, future.isDone()));
+		}
+
+		try {
+			executorService.awaitTermination(10, TimeUnit.SECONDS);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@Test
+	public void noCatchUp() {
+		int poolSize = 1;
+		ScheduledExecutorService executorService = Executors.newScheduledThreadPool(poolSize);
+		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		System.out.println(String.format("%s T[%d] init", df.format(new Date()), Thread.currentThread().getId()));
+		long start = System.currentTimeMillis();
+		ScheduledFuture<?> futureA = executorService.scheduleWithFixedDelay(() -> {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			try {
+				System.out.println(String.format("%s T[%d] %d task: A ready", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start));
+				TimeUnit.SECONDS.sleep(7);
+				System.out.println(String.format("%s T[%d] %d task: A finished", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start));
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+				Thread.currentThread().interrupt();
+			}
+		}, 1_000, 1_000, TimeUnit.MILLISECONDS);
+
+		ScheduledFuture<?> futureB = executorService.scheduleWithFixedDelay(() -> {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			try {
+				System.out.println(String.format("%s T[%d] %d task: B ready", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start));
+				System.out.println(String.format("%s T[%d] %d task: B finished", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start));
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}, 1_000, 1_000, TimeUnit.MILLISECONDS);
+
+		try {
+			TimeUnit.SECONDS.sleep(4);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+
+		if (false == futureA.isDone()) {
+			futureA.cancel(true);
+		}
+
+		try {
+			executorService.awaitTermination(10, TimeUnit.SECONDS);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@Test
+	public void catchUp() {
+		int poolSize = 1;
+		ScheduledExecutorService executorService = Executors.newScheduledThreadPool(poolSize);
+		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		System.out.println(String.format("%s T[%d] init", df.format(new Date()), Thread.currentThread().getId()));
+		long start = System.currentTimeMillis();
+		ScheduledFuture<?> futureA = executorService.scheduleAtFixedRate(() -> {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			try {
+				System.out.println(String.format("%s T[%d] %d task: A ready", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start));
+				TimeUnit.SECONDS.sleep(7);
+				System.out.println(String.format("%s T[%d] %d task: A finished", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start));
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+				Thread.currentThread().interrupt();
+			}
+		}, 1_000, 1_000, TimeUnit.MILLISECONDS);
+
+		ScheduledFuture<?> futureB = executorService.scheduleAtFixedRate(() -> {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			try {
+				System.out.println(String.format("%s T[%d] %d task: B ready", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start));
+				System.out.println(String.format("%s T[%d] %d task: B finished", sdf.format(new Date()),
+						Thread.currentThread().getId(), System.currentTimeMillis() - start));
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}, 1_000, 1_000, TimeUnit.MILLISECONDS);
+
+		try {
+			TimeUnit.SECONDS.sleep(4);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+
+		if (false == futureA.isDone()) {
+			futureA.cancel(true);
+		}
+
+		try {
+			executorService.awaitTermination(10, TimeUnit.SECONDS);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
