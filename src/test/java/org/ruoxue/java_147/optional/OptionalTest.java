3cdfb4c82f8a010fbc1bdb5282258cfd8f88eb9e
ruoxue <ruoxue777@gmail.com>
2023-03-19 14:12:58 +0800

    u

--- src/test/java/org/ruoxue/java_147/optional/OptionalTest.java
+++ src/test/java/org/ruoxue/java_147/optional/OptionalTest.java
@@ -66,6 +66,10 @@ public class OptionalTest {
 		food.ifPresent(e -> System.out.println(e + ", " + e.length()));
 		assertTrue(food.isPresent());
 
+		food = Optional.ofNullable("Beef");
+		food.ifPresent(System.out::println);
+		assertTrue(food.isPresent());
+
 		food = Optional.ofNullable(null);
 		food.ifPresent(e -> System.out.println(e + ", " + e.length()));
 		assertFalse(food.isPresent());
@@ -79,49 +83,126 @@ public class OptionalTest {
 	@Test
 	public void orElse() {
 		Optional<String> food = Optional.ofNullable("Beef");
-		String value = food.orElse("defaultValue");
-		System.out.println(value);
+		String result = food.orElse("defaultValue");
+		System.out.println(result);
 		assertTrue(food.isPresent());
 
 		food = Optional.ofNullable(null);
-		value = food.orElse("defaultValue");
-		System.out.println(value);
+		result = food.orElse("defaultValue");
+		System.out.println(result);
 		assertFalse(food.isPresent());
 
 		food = Optional.ofNullable(null);
-		value = food.orElse(getDefaultValue());
-		System.out.println(value);
+		result = food.orElse(getDefaultValue());
+		System.out.println(result);
 		assertFalse(food.isPresent());
 	}
 
 	@Test
 	public void orElseGet() {
 		Optional<String> food = Optional.ofNullable("Beef");
-		String value = food.orElseGet(() -> "defaultValue");
-		System.out.println(value);
+		String result = food.orElseGet(() -> "defaultValue");
+		System.out.println(result);
 		assertTrue(food.isPresent());
 
 		food = Optional.ofNullable(null);
-		value = food.orElseGet(() -> "defaultValue");
-		System.out.println(value);
+		result = food.orElseGet(() -> "defaultValue");
+		System.out.println(result);
 		assertFalse(food.isPresent());
 
 		food = Optional.ofNullable(null);
-		value = food.orElseGet(this::getDefaultValue);
-		System.out.println(value);
+		result = food.orElseGet(this::getDefaultValue);
+		System.out.println(result);
 		assertFalse(food.isPresent());
 	}
 
 	@Test
 	public void orElse_orElseGet() {
 		Optional<String> food = Optional.ofNullable("Beef");
-		String value = food.orElse(getDefaultValue());
-		System.out.println(value);
+		String result = food.orElse(getDefaultValue());
+		System.out.println(result);
 		assertTrue(food.isPresent());
 
 		food = Optional.ofNullable("Beef");
-		value = food.orElseGet(this::getDefaultValue);
-		System.out.println(value);
+		result = food.orElseGet(this::getDefaultValue);
+		System.out.println(result);
 		assertTrue(food.isPresent());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void orElseThrow() {
+		Optional<String> food = Optional.ofNullable(null);
+		String result = food.orElseThrow(IllegalArgumentException::new);
+		System.out.println(result);
+		assertFalse(food.isPresent());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void findById() {
+		String result = findById_orElseThrow("Beef");
+		System.out.println(result);
+		assertNotNull(result);
+
+		result = findById_orElseThrow(null);
+		System.out.println(result);
+		assertNull(result);
+	}
+
+	public String findById(String id) {
+		if (id != null) {
+			return id;
+		} else {
+			throw new IllegalArgumentException(id);
+		}
+	}
+
+	public String findById_optional(String id) {
+		Optional<String> optional = Optional.ofNullable(id);
+		if (optional.isPresent()) {
+			return optional.get();
+		} else {
+			throw new IllegalArgumentException(id);
+		}
+	}
+
+	public String findById_orElseThrow(String id) {
+		Optional<String> optional = Optional.ofNullable(id);
+		return optional.orElseThrow(() -> new IllegalArgumentException(id));
+	}
+
+	@Test
+	public void filter() {
+		Optional<String> food = Optional.ofNullable("Beef");
+		Optional<String> optional = food.filter(e -> e.contains("B"));
+		System.out.println(optional);
+		boolean result = optional.isPresent();
+		System.out.println(result);
+		assertTrue(result);
+
+		food = Optional.ofNullable(null);
+		optional = food.filter(e -> e.contains("B"));
+		System.out.println(optional);
+		assertEquals(Optional.empty(), optional);
+		result = optional.isPresent();
+		System.out.println(result);
+		assertFalse(result);
+	}
+
+	@Test
+	public void map() {
+		Optional<String> food = Optional.ofNullable("Beef");
+		Optional<Integer> optional = food.map(e -> e.length());
+		System.out.println(optional);
+		int result = optional.orElse(0);
+		System.out.println(result);
+		assertEquals(4, result);
+
+		food = Optional.ofNullable(null);
+		optional = food.map(String::length);
+		System.out.println(optional);
+		assertEquals(Optional.empty(), optional);
+		result = optional.orElse(0);
+		System.out.println(result);
+		assertEquals(0, result);
 	}
 }
