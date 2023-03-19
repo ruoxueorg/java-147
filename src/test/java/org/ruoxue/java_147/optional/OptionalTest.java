4d2cf3ff36bd74c36b50b0682558d2536df9da95
ruoxue <ruoxue777@gmail.com>
2023-03-19 13:25:18 +0800

    u

--- src/test/java/org/ruoxue/java_147/optional/OptionalTest.java
+++ src/test/java/org/ruoxue/java_147/optional/OptionalTest.java
@@ -2,6 +2,7 @@ package org.ruoxue.java_147.optional;
 
 import static org.junit.Assert.*;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.junit.Test;
@@ -37,19 +38,90 @@ public class OptionalTest {
 
 	@Test
 	public void ofNullable() {
+		Optional<String> food = Optional.ofNullable("Beef");
+		System.out.println(food.get());
+		assertTrue(food.isPresent());
+
+		food = Optional.ofNullable(null);
+		assertFalse(food.isPresent());
+	}
+
+	@Test
+	public void get() {
+		Optional<String> food = Optional.ofNullable("Beef");
+		System.out.println(food.get());
+		assertTrue(food.isPresent());
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void getThrowException() {
 		Optional<String> food = Optional.ofNullable(null);
+		System.out.println(food.get());
 		assertFalse(food.isPresent());
 	}
 
 	@Test
 	public void ifPresent() {
 		Optional<String> food = Optional.ofNullable("Beef");
-		food.ifPresent(e -> System.out.println(e.length()));
+		food.ifPresent(e -> System.out.println(e + ", " + e.length()));
 		assertTrue(food.isPresent());
 
 		food = Optional.ofNullable(null);
-		food.ifPresent(e -> System.out.println(e.length()));
+		food.ifPresent(e -> System.out.println(e + ", " + e.length()));
 		assertFalse(food.isPresent());
 	}
 
+	public String getDefaultValue() {
+		System.out.println("call getDefaultValue()");
+		return "defaultValue";
+	}
+
+	@Test
+	public void orElse() {
+		Optional<String> food = Optional.ofNullable("Beef");
+		String value = food.orElse("defaultValue");
+		System.out.println(value);
+		assertTrue(food.isPresent());
+
+		food = Optional.ofNullable(null);
+		value = food.orElse("defaultValue");
+		System.out.println(value);
+		assertFalse(food.isPresent());
+
+		food = Optional.ofNullable(null);
+		value = food.orElse(getDefaultValue());
+		System.out.println(value);
+		assertFalse(food.isPresent());
+	}
+
+	@Test
+	public void orElseGet() {
+		Optional<String> food = Optional.ofNullable("Beef");
+		String value = food.orElseGet(() -> "defaultValue");
+		System.out.println(value);
+		assertTrue(food.isPresent());
+
+		food = Optional.ofNullable(null);
+		value = food.orElseGet(() -> "defaultValue");
+		System.out.println(value);
+		assertFalse(food.isPresent());
+
+		food = Optional.ofNullable(null);
+		value = food.orElseGet(this::getDefaultValue);
+		System.out.println(value);
+		assertFalse(food.isPresent());
+	}
+
+	@Test
+	public void orElse_orElseGet() {
+		Optional<String> food = Optional.ofNullable("Beef");
+		String value = food.orElse(getDefaultValue());
+		System.out.println(value);
+		assertTrue(food.isPresent());
+
+		food = Optional.ofNullable("Beef");
+		value = food.orElseGet(this::getDefaultValue);
+		System.out.println(value);
+		assertTrue(food.isPresent());
+	}
 }
