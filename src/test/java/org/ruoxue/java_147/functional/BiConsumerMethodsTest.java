95c2759d4113be1b025780f28a598a903a151382
ruoxue <ruoxue777@gmail.com>
2023-04-04 22:19:11 +0800

    add BiConsumerMethodsTest

---
+++ src/test/java/org/ruoxue/java_147/functional/BiConsumerMethodsTest.java
@@ -0,0 +1,66 @@
+package org.ruoxue.java_147.functional;
+
+import java.util.function.BiConsumer;
+
+import org.junit.Test;
+
+public class BiConsumerMethodsTest {
+
+	@Test
+	public void accept() {
+		BiConsumer<String, String> startsWith = (s, s2) -> System.out.println(s.startsWith(s2));
+		startsWith.accept("Bacon", "B");
+		startsWith.accept("Ham", "B");
+
+		BiConsumer<String, Integer> greaterThan = (s, i) -> System.out.println(s.length() > i);
+		greaterThan.accept("Bacon", 3);
+		greaterThan.accept("Ham", 3);
+	}
+
+	@Test
+	public void andThen() {
+		BiConsumer<String, String> startsWith = (s, s2) -> System.out.println(s.startsWith(s2));
+		BiConsumer<String, String> endsWith = (s, s2) -> System.out.println(s.endsWith(s2));
+		startsWith.andThen(endsWith).accept("BaconB", "B");
+		startsWith.andThen(endsWith).accept("Ham", "B");
+
+		BiConsumer<String, Integer> greaterThan = (s, i) -> System.out.println(s.length() > i);
+		BiConsumer<String, Integer> mod = (s, i) -> System.out.println(s.length() % i == 1);
+		greaterThan.andThen(mod).accept("BaconB", 5);
+		greaterThan.andThen(mod).accept("Ham", 6);
+	}
+
+	@Test
+	public void chaining() {
+		BiConsumer<String, String> contains = (s, s2) -> System.out.println(s.contains(s2));
+		BiConsumer<String, String> startsWith = (s, s2) -> System.out.println(s.startsWith(s2));
+		BiConsumer<String, String> endsWith = (s, s2) -> System.out.println(s.endsWith(s2));
+		contains.andThen(startsWith).andThen(endsWith).accept("Bacon", "B");
+		contains.andThen(startsWith).andThen(endsWith).accept("Ham", "B");
+
+		BiConsumer<String, Integer> parseInt = (s, i) -> {
+			if (i < Integer.parseInt(s))
+				System.out.println(true);
+			System.out.println(false);
+		};
+		BiConsumer<String, Integer> greaterThan = (s, i) -> System.out.println(s.length() > i);
+		BiConsumer<String, Integer> mod = (s, i) -> System.out.println(s.length() % i == 1);
+		parseInt.andThen(greaterThan).andThen(mod).accept("777", 2);
+		parseInt.andThen(greaterThan).andThen(mod).accept("7", 2);
+	}
+
+	public static class LengthGreaterThan<E> implements BiConsumer<String, Integer> {
+		@Override
+		public void accept(String t, Integer u) {
+			System.out.println(t.length() > u);
+		}
+	}
+
+	@Test
+	public void traditional() {
+		BiConsumer<String, Integer> lengthGreaterThan = new LengthGreaterThan<String>();
+		BiConsumer<String, Integer> mod = (s, i) -> System.out.println(s.length() % i == 1);
+		lengthGreaterThan.andThen(mod).accept("Bacon", 4);
+		lengthGreaterThan.andThen(mod).accept("Ham", 3);
+	}
+}
--- src/test/java/org/ruoxue/java_147/functional/README.md
+++ src/test/java/org/ruoxue/java_147/functional/README.md
@@ -13,3 +13,7 @@
 10. [Consumer in Java with Examples](https://www.ruoxue.org/java-147-consumer-in-java-with-examples/)
 11. [Java Consumer Interface](https://www.ruoxue.org/java-147-java-consumer-interface/)
 12. [Consumer Functional Interface in Java](https://www.ruoxue.org/java-147-consumer-functional-interface-in-java/)
+13. [Java BiConsumer Methods](https://www.ruoxue.org/java-147-java-biconsumer-methods/)
+14. [BiConsumer in Java with Examples](https://www.ruoxue.org/java-147-biconsumer-in-java-with-examples/)
+15. [Java BiConsumer Interface](https://www.ruoxue.org/java-147-java-biconsumer-interface/)
+16. [BiConsumer Functional Interface in Java](https://www.ruoxue.org/java-147-biconsumer-functional-interface-in-java/)
