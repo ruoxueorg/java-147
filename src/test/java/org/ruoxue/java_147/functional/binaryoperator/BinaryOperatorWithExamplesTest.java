d5c768517e078f719ef7ab8339abcd0ad573586e
ruoxue <ruoxue777@gmail.com>
2023-04-27 17:54:54 +0800

    update

---
+++ src/test/java/org/ruoxue/java_147/functional/binaryoperator/BinaryOperatorWithExamplesTest.java
@@ -0,0 +1,128 @@
+package org.ruoxue.java_147.functional.binaryoperator;
+
+import static org.junit.Assert.*;
+
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.junit.Test;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class BinaryOperatorWithExamplesTest {
+
+	@NoArgsConstructor
+	@Getter
+	@Setter
+	@Builder
+	public static class Food {
+		private String name;
+		private double quantity;
+		private int type;
+
+		public Food(String name, double quantity, int type) {
+			this.name = name;
+			this.quantity = quantity;
+			this.type = type;
+		}
+
+		public String toString() {
+			ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.JSON_STYLE);
+			builder.appendSuper(super.toString());
+			builder.append("name", name);
+			builder.append("quantity", quantity);
+			builder.append("type", type);
+			return builder.toString();
+		}
+
+		public boolean equals(Object object) {
+			if (!(object instanceof Food)) {
+				return false;
+			}
+			if (this == object) {
+				return true;
+			}
+			Food other = (Food) object;
+			return new EqualsBuilder().append(getName(), other.getName()).isEquals();
+		}
+
+		public int hashCode() {
+			return new HashCodeBuilder().append(getName()).toHashCode();
+		}
+	}
+
+	@Test
+	public void apply() {
+		BinaryOperator<Food> merge = (o, o1) -> {
+			Food result = new Food();
+			result.setName(o.name.concat(o1.name));
+			result.setQuantity(o.quantity + o1.quantity);
+			result.setType(o.type + o1.type);
+			return result;
+		};
+		Food food = new Food("Bacon", 1, 1);
+		Food food2 = new Food("Ham", 2, 1);
+		Food result = merge.apply(food, food2);
+		System.out.println(result);
+		assertEquals("BaconHam", result.getName());
+
+		Food food3 = new Food("Pork", 3, 1);
+		result = merge.apply(food2, food3);
+		System.out.println(result);
+		assertEquals(5d, result.getQuantity(), 2);
+	}
+
+//	@Test
+//	public void andThen() {
+//		BiFunction<Food, String, String> concat = (o, s) -> o.name.concat(s);
+//		Function<String, Integer> multiply = s -> s.length() * 2;
+//		Food food = new Food("Bacon", 1, 1);
+//		int result = concat.andThen(multiply).apply(food, "B");
+//		System.out.println(result);
+//		assertEquals(12, result);
+//		food = new Food("Ham", 3, 1);
+//		result = concat.andThen(multiply).apply(food, "B");
+//		System.out.println(result);
+//		assertEquals(8, result);
+//	}
+
+	@Test(expected = NullPointerException.class)
+	public void andThenThrowException() {
+		BinaryOperator<Food> ret = (o, o2) -> o2;
+		ret.andThen(null);
+	}
+
+	public static class Merge<E> implements BinaryOperator<Food> {
+		@Override
+		public Food apply(Food t, Food u) {
+			Food result = new Food();
+			result.setName(t.name.concat(u.name));
+			result.setQuantity(t.quantity + u.quantity);
+			result.setType(t.type + u.type);
+			return result;
+		}
+	}
+
+	@Test
+	public void traditional() {
+		BinaryOperator<Food> merge = new Merge<>();
+		Function<Food, Integer> multiply = s -> s.name.length() * 2;
+		Food food = new Food("Bacon", 1, 1);
+		Food food2 = new Food("Ham", 2, 1);
+		int result = merge.andThen(multiply).apply(food, food2);
+		System.out.println(result);
+		assertEquals(16, result);
+
+		Food food3 = new Food("Pork", 3, 1);
+		result = merge.andThen(multiply).apply(food2, food3);
+		System.out.println(result);
+		assertEquals(14, result);
+	}
+}
