363b7923eb34dc98418657e0474a0fd5e4af8ba7
ruoxue <ruoxue777@gmail.com>
2023-05-25 15:57:10 +0800

    add test

---
+++ src/test/java/org/ruoxue/java_147/map/InitializeTreeSetTest.java
@@ -0,0 +1,147 @@
+package org.ruoxue.java_147.map;
+
+import static org.junit.Assert.*;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.junit.Test;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class InitializeTreeSetTest {
+
+	@NoArgsConstructor
+	@Getter
+	@Setter
+	@Builder
+	public static class Fruit implements Comparable<Fruit> {
+
+		private String name;
+		private double quantity;
+		private int type;
+
+		public Fruit(String name, double quantity, int type) {
+			this.name = name;
+			this.quantity = quantity;
+			this.type = type;
+		}
+
+		public String toString() {
+			ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.JSON_STYLE);
+			builder.appendSuper(super.toString());
+			builder.append("name", name);
+			builder.append("quantity", quantity);
+			builder.append("type", type);
+			return builder.toString();
+		}
+
+		public boolean equals(Object object) {
+			if (!(object instanceof Fruit)) {
+				return false;
+			}
+			if (this == object) {
+				return true;
+			}
+			Fruit other = (Fruit) object;
+			return new EqualsBuilder().append(getName(), other.getName()).isEquals();
+		}
+
+		public int hashCode() {
+			return new HashCodeBuilder().append(getName()).toHashCode();
+		}
+
+		@Override
+		public int compareTo(Fruit o) {
+			return this.name.compareTo(o.name);
+		}
+	}
+
+	@Test
+	public void add() {
+		int expectedSize = 3;
+		Set<Fruit> set = new TreeSet<Fruit>();
+		set.add(new Fruit("Longan", 1, 1));
+		set.add(new Fruit("Tomato", 3, 1));
+		set.add(new Fruit("Pear", 2, 1));
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void doubleBrace() {
+		int expectedSize = 3;
+		Set<Fruit> set = new TreeSet<Fruit>() {
+			private static final long serialVersionUID = -1234223135233714632L;
+			{
+				add(new Fruit("Longan", 1, 1));
+				add(new Fruit("Tomato", 3, 1));
+				add(new Fruit("Pear", 2, 1));
+			}
+		};
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void addAll() {
+		int expectedSize = 3;
+		Set<Fruit> set = new TreeSet<Fruit>();
+		set.add(new Fruit("Longan", 1, 1));
+		set.add(new Fruit("Tomato", 3, 1));
+		set.add(new Fruit("Pear", 2, 1));
+		Set<Fruit> newSet = new TreeSet<Fruit>();
+		newSet.addAll(set);
+		System.out.println(newSet);
+		assertEquals(expectedSize, newSet.size());
+	}
+
+	@Test
+	public void constructor() {
+		int expectedSize = 3;
+		Set<Fruit> set = new TreeSet<Fruit>();
+		set.add(new Fruit("Longan", 1, 1));
+		set.add(new Fruit("Tomato", 3, 1));
+		set.add(new Fruit("Pear", 2, 1));
+		Set<Fruit> newSet = new TreeSet<Fruit>(set);
+		System.out.println(newSet);
+		assertEquals(expectedSize, newSet.size());
+	}
+
+	@Test
+	public void comparator() {
+		int expectedSize = 3;
+		Comparator<Fruit> quantityComparator = (o, o2) -> Double.compare(o.quantity, o2.quantity);
+		Set<Fruit> set = new TreeSet<Fruit>(quantityComparator);
+		set.add(new Fruit("Longan", 1, 1));
+		set.add(new Fruit("Tomato", 3, 1));
+		set.add(new Fruit("Pear", 2, 1));
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void sortedMap() {
+		int expectedSize = 3;
+		SortedSet<Fruit> set = new ConcurrentSkipListSet<Fruit>();
+		set.add(new Fruit("Longan", 1, 1));
+		set.add(new Fruit("Tomato", 3, 1));
+		set.add(new Fruit("Pear", 2, 1));
+		Set<Fruit> newSet = new TreeSet<>(set);
+		System.out.println(newSet);
+		assertEquals(expectedSize, newSet.size());
+	}
+}
--- src/test/java/org/ruoxue/java_147/set/README.md
+++ src/test/java/org/ruoxue/java_147/set/README.md
@@ -7,3 +7,7 @@
 4. [Java LinkedHashSet Methods](https://www.ruoxue.org/java-147-java-linkedhashset-methods/)
 5. [LinkedHashSet in Java with Examples](https://www.ruoxue.org/java-147-linkedhashset-in-java-with-examples/)
 6. [Java LinkedHashSet Class](https://www.ruoxue.org/java-147-java-linkedhashset-class/)
+7. [Java TreeSet Methods](https://www.ruoxue.org/java-147-java-treeset-methods/)
+8. [TreeSet in Java with Examples](https://www.ruoxue.org/java-147-treeset-in-java-with-examples/)
+9. [Java TreeSet Class](https://www.ruoxue.org/java-147-java-treeset-class/)
+10. [Initialize TreeSet in Java](https://www.ruoxue.org/java-147-initialize-treeset-in-java/)
