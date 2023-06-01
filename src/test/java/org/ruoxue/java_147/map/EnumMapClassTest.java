b4c74c01edb8d8788e693b12e4e3db0f86494803
cheng <cheng@gmail.com>
2023-06-01 17:56:30 +0800

    add EnumMapComputeTest

---
+++ src/test/java/org/ruoxue/java_147/map/EnumMapClassTest.java
@@ -0,0 +1,114 @@
+package org.ruoxue.java_147.map;
+
+import static org.junit.Assert.*;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+public class EnumMapClassTest {
+
+	public enum Fruit {
+		GRAPE,
+		KIWIFRUIT,
+		LEMON,
+		
+		EMPTY,
+		
+		APPLE,
+		BANANA,
+		CHERRY
+	}
+	
+	@Test
+	public void containsKey() {
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		boolean containsKey = map.containsKey(Fruit.LEMON);
+		System.out.println(containsKey);
+		assertTrue(containsKey);
+	}
+
+	@Test
+	public void containsValue() {
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		boolean containsValue = map.containsValue(3);
+		System.out.println(containsValue);
+		assertTrue(containsValue);
+	}
+
+	@Test
+	public void stream() {
+		int expectedSize = 2;
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		Set<Fruit> set = map.keySet().stream().filter(e -> e.name().length() < 6).collect(Collectors.toSet());
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void parallelStream() {
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		map.keySet().parallelStream().forEach(System.out::println);
+		System.out.println("----------");
+		map.keySet().parallelStream().forEachOrdered(System.out::println);
+	}
+
+	@Test
+	public void replace() {
+		Integer expected = 1;
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		Integer replaced = map.replace(Fruit.GRAPE, 10);
+		System.out.println(map);
+		assertEquals(expected, replaced);
+		boolean repl = map.replace(Fruit.GRAPE, 10, 1);
+		System.out.println(map);
+		assertEquals(true, repl);
+	}
+
+	@Test
+	public void replaceAll() {
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		map.replaceAll((k, v) -> {
+			v = v * 10;
+			return v;
+		});
+		System.out.println(map);
+	}
+
+	@Test
+	public void merge() {
+		Integer expected = 11;
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		Integer replaced = map.merge(Fruit.GRAPE, 10, (oldValue, newValue) -> oldValue + newValue);
+		System.out.println(map);
+		assertEquals(expected, replaced);
+
+		replaced = map.merge(Fruit.APPLE, 4, (oldValue, newValue) -> oldValue + newValue);
+		System.out.println(map);
+		assertEquals(4, replaced.intValue());
+	}
+}
---
+++ src/test/java/org/ruoxue/java_147/map/EnumMapComputeTest.java
@@ -0,0 +1,107 @@
+package org.ruoxue.java_147.map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class EnumMapComputeTest {
+
+	public enum Fruit {
+		GRAPE,
+		KIWIFRUIT,
+		LEMON,
+		
+		EMPTY,
+		
+		APPLE,
+		BANANA,
+		CHERRY
+	}
+	
+	@Test
+	public void compute() {
+		Integer expected = 2;
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		System.out.println(map);
+		Fruit key = Fruit.GRAPE;
+		map.compute(key, (k, v) -> v + 1);
+		System.out.println(map);
+		assertEquals(expected, map.get(key));
+	}
+
+	@Test
+	public void computeFunction() {
+		Integer expected = 2;
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		System.out.println(map);
+		Fruit key = Fruit.GRAPE;
+		map.compute(key, (k, v) -> {
+			v += 1;
+			return v;
+		});
+		System.out.println(map);
+		assertEquals(expected, map.get(key));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void computeNullPointerException() {
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		System.out.println(map);
+		Fruit key = null;
+		map.compute(key, (k, v) -> v + 1);
+		System.out.println(map);
+	}
+
+	@Test
+	public void computeIfAbsent() {
+		Integer expected = 4;
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		System.out.println(map);
+		Fruit key = Fruit.APPLE;
+		Integer result = map.computeIfAbsent(key, k -> 4);
+		System.out.println(result);
+		System.out.println(map);
+		assertEquals(expected, result);
+		key = Fruit.GRAPE;
+		result = map.computeIfAbsent(key, k -> 2);
+		System.out.println(result);
+		System.out.println(map);
+		assertEquals(1, result.intValue());
+	}
+
+	@Test
+	public void computeIfPresent() {
+		Integer expected = 2;
+		Map<Fruit, Integer> map = new EnumMap<>(Fruit.class);
+		map.put(Fruit.GRAPE, 1);
+		map.put(Fruit.KIWIFRUIT, 2);
+		map.put(Fruit.LEMON, 3);
+		System.out.println(map);
+		Fruit key = Fruit.GRAPE;
+		Integer result = map.computeIfPresent(key, (k, v) -> v + 1);
+		System.out.println(result);
+		System.out.println(map);
+		assertEquals(expected, result);
+		key = Fruit.APPLE;
+		result = map.computeIfPresent(key, (k, v) -> 4);
+		System.out.println(result);
+		System.out.println(map);
+		assertNull(result);
+	}
+}
