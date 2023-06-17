8dacae68db0e8669848cbf388b36c905212ce512
ruoxue <ruoxue777@gmail.com>
2023-06-17 14:12:41 +0800

    add InitializeLocalTimeTest

---
+++ src/test/java/org/ruoxue/java_147/datetime/localtime/InitializeLocalTimeTest.java
@@ -0,0 +1,73 @@
+package org.ruoxue.java_147.datetime.localtime;
+
+import static org.junit.Assert.*;
+
+import java.time.Clock;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Test;
+
+public class InitializeLocalTimeTest {
+
+	@Test
+	public void now() {
+		LocalTime localTime = LocalTime.now();
+		System.out.println(localTime);
+
+		ZoneId zone = ZoneId.of("Europe/London");
+		localTime = LocalTime.now(zone);
+		System.out.println(localTime);
+
+		zone = ZoneId.of("UTC-12");
+		Clock clock = Clock.system(zone);
+		localTime = LocalTime.now(clock);
+		System.out.println(localTime);
+	}
+
+	@Test
+	public void of() {
+		LocalTime localTime = LocalTime.of(9, 12, 5);
+		System.out.println(localTime);
+		assertEquals("09:12:05", localTime.toString());
+
+		int secondOfDay = localTime.toSecondOfDay();
+		System.out.println(secondOfDay);
+		assertEquals(33125L, secondOfDay);
+
+		localTime = LocalTime.of(9, 12, 5, 999);
+		System.out.println(localTime);
+		assertEquals("09:12:05.000000999", localTime.toString());
+
+		localTime = LocalTime.ofSecondOfDay(33127L);
+		System.out.println(localTime);
+		assertEquals("09:12:07", localTime.toString());
+	}
+
+	@Test
+	public void parse() {
+		LocalTime localTime = LocalTime.parse("09:12:05");
+		System.out.println(localTime);
+		assertEquals("09:12:05", localTime.toString());
+
+		localTime = LocalTime.parse("091205", DateTimeFormatter.ofPattern("HHmmss"));
+		System.out.println(localTime);
+		assertEquals("09:12:05", localTime.toString());
+	}
+
+	@Test
+	public void from() {
+		LocalTime localTime = LocalTime.parse("09:12:05");
+		LocalTime result = LocalTime.from(localTime);
+		System.out.println(result);
+		assertEquals("09:12:05", result.toString());
+	}
+
+	@Test
+	public void ofSecondOfDay() {
+		LocalTime localTime = LocalTime.ofSecondOfDay(33125L);
+		System.out.println(localTime);
+		assertEquals("09:12:05", localTime.toString());
+	}
+}
---
+++ src/test/java/org/ruoxue/java_147/datetime/localtime/LocalTimeClassTest.java
@@ -0,0 +1,115 @@
+package org.ruoxue.java_147.datetime.localtime;
+
+import static org.junit.Assert.*;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.OffsetTime;
+import java.time.ZoneOffset;
+
+import org.junit.Test;
+
+public class LocalTimeClassTest {
+
+	@Test
+	public void plus() {
+		LocalTime localTime = LocalTime.of(9, 12, 5);
+		LocalTime result = localTime.plusHours(1);
+		int hour = result.getHour();
+		System.out.println(hour);
+		assertEquals(10, hour);
+
+		result = localTime.plusMinutes(1);
+		int minute = result.getMinute();
+		System.out.println(minute);
+		assertEquals(13, minute);
+
+		result = localTime.plusSeconds(1);
+		int second = result.getSecond();
+		System.out.println(second);
+		assertEquals(6, second);
+	}
+
+	@Test
+	public void plusTemporalAmount() {
+		LocalTime localTime = LocalTime.of(9, 12, 5);
+		LocalTime result = localTime.plus(Duration.ofHours(1));
+		int hour = result.getHour();
+		System.out.println(hour);
+		assertEquals(10, hour);
+
+		result = localTime.plus(Duration.ofMinutes(1));
+		int minute = result.getMinute();
+		System.out.println(minute);
+		assertEquals(13, minute);
+
+		result = localTime.plus(Duration.ofSeconds(1));
+		int second = result.getSecond();
+		System.out.println(second);
+		assertEquals(6, second);
+	}
+
+	@Test
+	public void minus() {
+		LocalTime localTime = LocalTime.of(9, 12, 5);
+		LocalTime result = localTime.minusHours(1);
+		int hour = result.getHour();
+		System.out.println(hour);
+		assertEquals(8, hour);
+
+		result = localTime.minusMinutes(1);
+		int minute = result.getMinute();
+		System.out.println(minute);
+		assertEquals(11, minute);
+
+		result = localTime.minusSeconds(1);
+		int second = result.getSecond();
+		System.out.println(second);
+		assertEquals(4, second);
+	}
+
+	@Test
+	public void minusTemporalAmount() {
+		LocalTime localTime = LocalTime.of(9, 12, 5);
+		LocalTime result = localTime.minus(Duration.ofHours(1));
+		int hour = result.getHour();
+		System.out.println(hour);
+		assertEquals(8, hour);
+
+		result = localTime.minus(Duration.ofMinutes(1));
+		int minute = result.getMinute();
+		System.out.println(minute);
+		assertEquals(11, minute);
+
+		result = localTime.minus(Duration.ofSeconds(1));
+		int second = result.getSecond();
+		System.out.println(second);
+		assertEquals(4, second);
+	}
+
+	@Test
+	public void isBefore() {
+		LocalTime localTime = LocalTime.of(9, 12, 5);
+		LocalTime localTime2 = LocalTime.of(10, 12, 5);
+		boolean result = localTime.isBefore(localTime2);
+		System.out.println(result);
+		assertTrue(result);
+	}
+
+	@Test
+	public void isAfter() {
+		LocalTime localTime = LocalTime.of(9, 12, 5);
+		LocalTime localTime2 = LocalTime.of(8, 12, 5);
+		boolean result = localTime.isAfter(localTime2);
+		System.out.println(result);
+		assertTrue(result);
+	}
+
+	@Test
+	public void atOffset() {
+		LocalTime localTime = LocalTime.of(9, 12, 5);
+		OffsetTime result = localTime.atOffset(ZoneOffset.ofHours(-4));
+		System.out.println(result);
+		assertEquals("09:12:05-04:00", result.toString());
+	}
+}
