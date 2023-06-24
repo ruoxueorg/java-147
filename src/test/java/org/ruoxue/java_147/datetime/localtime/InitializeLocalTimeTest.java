5a43e3dd9c9c1decd16c73e3a49af42aa5b1e60a
ruoxue <ruoxue777@gmail.com>
2023-06-24 21:02:58 +0800

    update test

--- src/test/java/org/ruoxue/java_147/datetime/localtime/InitializeLocalTimeTest.java
+++ src/test/java/org/ruoxue/java_147/datetime/localtime/InitializeLocalTimeTest.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.*;
 
 import java.time.Clock;
 import java.time.LocalTime;
+import java.time.OffsetTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.junit.Test;
@@ -24,6 +27,10 @@ public class InitializeLocalTimeTest {
 		Clock clock = Clock.system(zone);
 		localTime = LocalTime.now(clock);
 		System.out.println(localTime);
+
+		zone = ZoneId.of("Etc/GMT-1");
+		localTime = LocalTime.now(zone);
+		System.out.println(localTime);
 	}
 
 	@Test
@@ -36,9 +43,9 @@ public class InitializeLocalTimeTest {
 		System.out.println(secondOfDay);
 		assertEquals(33125L, secondOfDay);
 
-		localTime = LocalTime.of(9, 12, 5, 999);
+		localTime = LocalTime.of(9, 12, 5, 123456789);
 		System.out.println(localTime);
-		assertEquals("09:12:05.000000999", localTime.toString());
+		assertEquals("09:12:05.123456789", localTime.toString());
 
 		localTime = LocalTime.ofSecondOfDay(33127L);
 		System.out.println(localTime);
@@ -54,14 +61,32 @@ public class InitializeLocalTimeTest {
 		localTime = LocalTime.parse("091205", DateTimeFormatter.ofPattern("HHmmss"));
 		System.out.println(localTime);
 		assertEquals("09:12:05", localTime.toString());
+
+		localTime = LocalTime.parse("09-12-05.123", DateTimeFormatter.ofPattern("HH-mm-ss.SSS"));
+		System.out.println(localTime);
+		assertEquals("09:12:05.123", localTime.toString());
 	}
 
 	@Test
 	public void from() {
-		LocalTime localTime = LocalTime.parse("09:12:05");
+		LocalTime localTime = LocalTime.parse("09:12:05.123456789");
 		LocalTime result = LocalTime.from(localTime);
 		System.out.println(result);
+		assertEquals("09:12:05.123456789", result.toString());
+
+		localTime = LocalTime.parse("09:12:05.123");
+		ZoneOffset zoneOffset = ZoneOffset.of("+01:00");
+		OffsetTime offsetTime = OffsetTime.of(localTime, zoneOffset);
+		System.out.println(offsetTime);
+		result = LocalTime.from(offsetTime);
+		System.out.println(result);
+		assertEquals("09:12:05.123", result.toString());
+
+		ZonedDateTime zonedDateTime = ZonedDateTime.parse("2023-12-25T09:12:05+01:00[Europe/London]");
+		result = LocalTime.from(zonedDateTime);
+		System.out.println(result);
 		assertEquals("09:12:05", result.toString());
+
 	}
 
 	@Test
@@ -69,5 +94,9 @@ public class InitializeLocalTimeTest {
 		LocalTime localTime = LocalTime.ofSecondOfDay(33125L);
 		System.out.println(localTime);
 		assertEquals("09:12:05", localTime.toString());
+
+		localTime = LocalTime.ofNanoOfDay(33125123456789L);
+		System.out.println(localTime);
+		assertEquals("09:12:05.123456789", localTime.toString());
 	}
 }
