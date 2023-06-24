fb4205b0b4ed01d9bd87c7d9ea8ebffc4b622f82
ruoxue <ruoxue777@gmail.com>
2023-06-24 21:27:44 +0800

    update from, add ofNanoOfDay

--- src/test/java/org/ruoxue/java_147/datetime/localtime/LocalTimeWithExamplesTest.java
+++ src/test/java/org/ruoxue/java_147/datetime/localtime/LocalTimeWithExamplesTest.java
@@ -3,6 +3,9 @@ package org.ruoxue.java_147.datetime.localtime;
 import static org.junit.Assert.*;
 
 import java.time.LocalTime;
+import java.time.OffsetTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
@@ -44,8 +47,21 @@ public class LocalTimeWithExamplesTest {
 
 	@Test
 	public void from() {
-		LocalTime localTime = LocalTime.parse("09:12:05");
+		LocalTime localTime = LocalTime.parse("09:12:05.123456789");
 		LocalTime result = LocalTime.from(localTime);
+		System.out.println(result);
+		assertEquals("09:12:05.123456789", result.toString());
+
+		localTime = LocalTime.parse("09:12:05.123");
+		ZoneOffset zoneOffset = ZoneOffset.of("+01:00");
+		OffsetTime offsetTime = OffsetTime.of(localTime, zoneOffset);
+		System.out.println(offsetTime);
+		result = LocalTime.from(offsetTime);
+		System.out.println(result);
+		assertEquals("09:12:05.123", result.toString());
+
+		ZonedDateTime zonedDateTime = ZonedDateTime.parse("2023-12-25T09:12:05+01:00[Europe/London]");
+		result = LocalTime.from(zonedDateTime);
 		System.out.println(result);
 		assertEquals("09:12:05", result.toString());
 	}
@@ -66,6 +82,13 @@ public class LocalTimeWithExamplesTest {
 	}
 
 	@Test
+	public void ofNanoOfDay() {
+		LocalTime localTime = LocalTime.ofNanoOfDay(33125123456789L);
+		System.out.println(localTime);
+		assertEquals("09:12:05.123456789", localTime.toString());
+	}
+
+	@Test
 	public void truncatedTo() {
 		LocalTime localTime = LocalTime.of(9, 12, 5, 123456789);
 		LocalTime result = localTime.truncatedTo(ChronoUnit.HOURS);
@@ -75,7 +98,7 @@ public class LocalTimeWithExamplesTest {
 		result = localTime.truncatedTo(ChronoUnit.MINUTES);
 		System.out.println(result);
 		assertEquals("09:12", result.toString());
-		
+
 		result = localTime.truncatedTo(ChronoUnit.MILLIS);
 		System.out.println(result);
 		assertEquals("09:12:05.123", result.toString());
