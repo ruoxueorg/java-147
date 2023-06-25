505d85f66af1be45420d40f4b12b52bfbd92c4ad
ruoxue <ruoxue777@gmail.com>
2023-06-25 12:50:07 +0800

    update parse, from, ofYearDay

--- src/test/java/org/ruoxue/java_147/datetime/localdate/InitializeLocalDateTest.java
+++ src/test/java/org/ruoxue/java_147/datetime/localdate/InitializeLocalDateTest.java
@@ -4,8 +4,12 @@ import static org.junit.Assert.*;
 
 import java.time.Clock;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.junit.Test;
@@ -24,6 +28,10 @@ public class InitializeLocalDateTest {
 		zone = ZoneId.of("UTC-5");
 		Clock clock = Clock.system(zone);
 		localDate = LocalDate.now(clock);
+		System.out.println(localDate);
+
+		zone = ZoneId.of("Etc/GMT+5");
+		localDate = LocalDate.now(zone);
 		System.out.println(localDate);
 	}
 
@@ -52,6 +60,10 @@ public class InitializeLocalDateTest {
 		System.out.println(localDate);
 		assertEquals("2023-06-18", localDate.toString());
 
+		localDate = LocalDate.parse("20230618", DateTimeFormatter.ofPattern("yyyyMMdd"));
+		System.out.println(localDate);
+		assertEquals("2023-06-18", localDate.toString());
+
 		localDate = LocalDate.parse("2023/06/18", DateTimeFormatter.ofPattern("yyyy/MM/dd"));
 		System.out.println(localDate);
 		assertEquals("2023-06-18", localDate.toString());
@@ -63,11 +75,31 @@ public class InitializeLocalDateTest {
 		LocalDate result = LocalDate.from(localDate);
 		System.out.println(result);
 		assertEquals("2023-06-18", result.toString());
+
+		LocalDateTime localDateTime = LocalDateTime.parse("2023-06-18T09:12:05");
+		ZoneOffset zoneOffset = ZoneOffset.of("+05:00");
+		OffsetDateTime offsetDateTime = OffsetDateTime.of(localDateTime, zoneOffset);
+		System.out.println(offsetDateTime);
+		result = LocalDate.from(offsetDateTime);
+		System.out.println(result);
+		assertEquals("2023-06-18", result.toString());
+
+		ZonedDateTime zonedDateTime = ZonedDateTime.parse("2023-06-18T09:12:05+05:00[America/Chicago]");
+		result = LocalDate.from(zonedDateTime);
+		System.out.println(result);
+		assertEquals("2023-06-18", result.toString());
 	}
 
 	@Test
 	public void ofEpochDay() {
 		LocalDate localDate = LocalDate.ofEpochDay(19526L);
+		System.out.println(localDate);
+		assertEquals("2023-06-18", localDate.toString());
+	}
+
+	@Test
+	public void ofYearDay() {
+		LocalDate localDate = LocalDate.ofYearDay(2023, 169);
 		System.out.println(localDate);
 		assertEquals("2023-06-18", localDate.toString());
 	}
--- src/test/java/org/ruoxue/java_147/datetime/localdate/LocalDateWithExamplesTest.java
+++ src/test/java/org/ruoxue/java_147/datetime/localdate/LocalDateWithExamplesTest.java
@@ -3,6 +3,10 @@ package org.ruoxue.java_147.datetime.localdate;
 import static org.junit.Assert.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.junit.Test;
@@ -27,15 +31,32 @@ public class LocalDateWithExamplesTest {
 		System.out.println(localDate);
 		assertEquals("2023-06-18", localDate.toString());
 
+		localDate = LocalDate.parse("20230618", DateTimeFormatter.ofPattern("yyyyMMdd"));
+		System.out.println(localDate);
+		assertEquals("2023-06-18", localDate.toString());
+
 		localDate = LocalDate.parse("2023/06/18", DateTimeFormatter.ofPattern("yyyy/MM/dd"));
 		System.out.println(localDate);
 		assertEquals("2023-06-18", localDate.toString());
 	}
-
+	
 	@Test
 	public void from() {
 		LocalDate localDate = LocalDate.parse("2023-06-18");
 		LocalDate result = LocalDate.from(localDate);
+		System.out.println(result);
+		assertEquals("2023-06-18", result.toString());
+
+		LocalDateTime localDateTime = LocalDateTime.parse("2023-06-18T09:12:05");
+		ZoneOffset zoneOffset = ZoneOffset.of("+05:00");
+		OffsetDateTime offsetDateTime = OffsetDateTime.of(localDateTime, zoneOffset);
+		System.out.println(offsetDateTime);
+		result = LocalDate.from(offsetDateTime);
+		System.out.println(result);
+		assertEquals("2023-06-18", result.toString());
+
+		ZonedDateTime zonedDateTime = ZonedDateTime.parse("2023-06-18T09:12:05+05:00[America/Chicago]");
+		result = LocalDate.from(zonedDateTime);
 		System.out.println(result);
 		assertEquals("2023-06-18", result.toString());
 	}
@@ -51,6 +72,13 @@ public class LocalDateWithExamplesTest {
 	@Test
 	public void ofEpochDay() {
 		LocalDate localDate = LocalDate.ofEpochDay(19526L);
+		System.out.println(localDate);
+		assertEquals("2023-06-18", localDate.toString());
+	}
+	
+	@Test
+	public void ofYearDay() {
+		LocalDate localDate = LocalDate.ofYearDay(2023, 169);
 		System.out.println(localDate);
 		assertEquals("2023-06-18", localDate.toString());
 	}
