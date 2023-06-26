4247355da3f8b7561c5269d614d96d8303a85b73
ruoxue <ruoxue777@gmail.com>
2023-06-26 20:52:27 +0800

    refactor predefined

--- src/test/java/org/ruoxue/java_147/datetime/datetimeformatter/DateTimeFormatterClassTest.java
+++ src/test/java/org/ruoxue/java_147/datetime/datetimeformatter/DateTimeFormatterClassTest.java
@@ -3,8 +3,15 @@ package org.ruoxue.java_147.datetime.datetimeformatter;
 import static org.junit.Assert.*;
 
 import java.text.Format;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.chrono.JapaneseChronology;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DecimalStyle;
+import java.time.format.ResolverStyle;
+import java.util.Locale;
 
 import org.junit.Test;
 
@@ -33,6 +40,77 @@ public class DateTimeFormatterClassTest {
 	}
 
 	@Test
+	public void predefined() {
+		LocalDateTime localDateTime = LocalDateTime.parse("2023-03-14T09:08:07.123456789");
+		OffsetDateTime offsetDateTime = OffsetDateTime.parse("2023-03-14T09:08:07.123456789+08:00");
+		ZonedDateTime zonedDateTime = ZonedDateTime.parse("2023-03-14T09:08:07.123456789+08:00[Asia/Taipei]");
+
+		String result = DateTimeFormatter.BASIC_ISO_DATE.format(localDateTime);
+		System.out.println("BASIC_ISO_DATE: " + result);
+		assertEquals("20230314", result);
+
+		result = DateTimeFormatter.ISO_LOCAL_DATE.format(localDateTime);
+		System.out.println("ISO_LOCAL_DATE: " + result);
+		assertEquals("2023-03-14", result);
+
+		result = DateTimeFormatter.ISO_OFFSET_DATE.format(offsetDateTime);
+		System.out.println("ISO_OFFSET_DATE: " + result);
+		assertEquals("2023-03-14+08:00", result);
+
+		result = DateTimeFormatter.ISO_DATE.format(localDateTime);
+		System.out.println("ISO_DATE: " + result);
+		assertEquals("2023-03-14", result);
+
+		result = DateTimeFormatter.ISO_LOCAL_TIME.format(localDateTime);
+		System.out.println("ISO_LOCAL_DATE: " + result);
+		assertEquals("09:08:07.123456789", result);
+
+		result = DateTimeFormatter.ISO_OFFSET_TIME.format(offsetDateTime);
+		System.out.println("ISO_OFFSET_DATE: " + result);
+		assertEquals("09:08:07.123456789+08:00", result);
+
+		result = DateTimeFormatter.ISO_TIME.format(localDateTime);
+		System.out.println("ISO_DATE: " + result);
+		assertEquals("09:08:07.123456789", result);
+
+		result = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(localDateTime);
+		System.out.println("ISO_LOCAL_DATE: " + result);
+		assertEquals("2023-03-14T09:08:07.123456789", result);
+
+		result = DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(offsetDateTime);
+		System.out.println("ISO_OFFSET_DATE: " + result);
+		assertEquals("2023-03-14T09:08:07.123456789+08:00", result);
+
+		result = DateTimeFormatter.ISO_DATE_TIME.format(localDateTime);
+		System.out.println("ISO_DATE_TIME: " + result);
+		assertEquals("2023-03-14T09:08:07.123456789", result);
+
+		result = DateTimeFormatter.ISO_DATE_TIME.format(zonedDateTime);
+		System.out.println("ISO_DATE_TIME: " + result);
+		assertEquals("2023-03-14T09:08:07.123456789+08:00[Asia/Taipei]", result);
+
+		result = DateTimeFormatter.ISO_ZONED_DATE_TIME.format(zonedDateTime);
+		System.out.println("ISO_ZONED_DATE_TIME: " + result);
+		assertEquals("2023-03-14T09:08:07.123456789+08:00[Asia/Taipei]", result);
+
+		result = DateTimeFormatter.ISO_ORDINAL_DATE.format(zonedDateTime);
+		System.out.println("ISO_ORDINAL_DATE: " + result);
+		assertEquals("2023-073+08:00", result);
+
+		result = DateTimeFormatter.ISO_WEEK_DATE.format(zonedDateTime);
+		System.out.println("ISO_WEEK_DATE: " + result);
+		assertEquals("2023-W11-2+08:00", result);
+
+		result = DateTimeFormatter.ISO_INSTANT.format(zonedDateTime);
+		System.out.println("ISO_INSTANT: " + result);
+		assertEquals("2023-03-14T01:08:07.123456789Z", result);
+
+		result = DateTimeFormatter.RFC_1123_DATE_TIME.format(zonedDateTime);
+		System.out.println("RFC_1123_DATE_TIME: " + result);
+		assertEquals("Tue, 14 Mar 2023 09:08:07 +0800", result);
+	}
+
+	@Test
 	public void get() {
 		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 		System.out.println(formatter.getChronology());
@@ -46,6 +124,14 @@ public class DateTimeFormatterClassTest {
 	@Test
 	public void with() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-	}
+		formatter.withChronology(JapaneseChronology.INSTANCE);
+		formatter.withDecimalStyle(DecimalStyle.STANDARD);
+		formatter.withLocale(Locale.TRADITIONAL_CHINESE);
+		formatter.withResolverStyle(ResolverStyle.LENIENT);
+		formatter.withZone(ZoneId.of("UTC+8"));
 
+		LocalDateTime localDateTime = LocalDateTime.now();
+		String result = formatter.format(localDateTime);
+		System.out.println(result);
+	}
 }
--- src/test/java/org/ruoxue/java_147/datetime/datetimeformatter/DateTimeFormatterMethodsTest.java
+++ src/test/java/org/ruoxue/java_147/datetime/datetimeformatter/DateTimeFormatterMethodsTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.OffsetDateTime;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Locale;
@@ -95,77 +93,6 @@ public class DateTimeFormatterMethodsTest {
 		result = formatter.format(localTime);
 		System.out.println(result);
 		assertEquals("09-08-07.123", result);
-	}
-
-	@Test
-	public void predefined() {
-		LocalDateTime localDateTime = LocalDateTime.parse("2023-03-14T09:08:07.123456789");
-		OffsetDateTime offsetDateTime = OffsetDateTime.parse("2023-03-14T09:08:07.123456789+08:00");
-		ZonedDateTime zonedDateTime = ZonedDateTime.parse("2023-03-14T09:08:07.123456789+08:00[Asia/Taipei]");
-
-		String result = DateTimeFormatter.BASIC_ISO_DATE.format(localDateTime);
-		System.out.println("BASIC_ISO_DATE: " + result);
-		assertEquals("20230314", result);
-
-		result = DateTimeFormatter.ISO_LOCAL_DATE.format(localDateTime);
-		System.out.println("ISO_LOCAL_DATE: " + result);
-		assertEquals("2023-03-14", result);
-
-		result = DateTimeFormatter.ISO_OFFSET_DATE.format(offsetDateTime);
-		System.out.println("ISO_OFFSET_DATE: " + result);
-		assertEquals("2023-03-14+08:00", result);
-
-		result = DateTimeFormatter.ISO_DATE.format(localDateTime);
-		System.out.println("ISO_DATE: " + result);
-		assertEquals("2023-03-14", result);
-
-		result = DateTimeFormatter.ISO_LOCAL_TIME.format(localDateTime);
-		System.out.println("ISO_LOCAL_DATE: " + result);
-		assertEquals("09:08:07.123456789", result);
-
-		result = DateTimeFormatter.ISO_OFFSET_TIME.format(offsetDateTime);
-		System.out.println("ISO_OFFSET_DATE: " + result);
-		assertEquals("09:08:07.123456789+08:00", result);
-
-		result = DateTimeFormatter.ISO_TIME.format(localDateTime);
-		System.out.println("ISO_DATE: " + result);
-		assertEquals("09:08:07.123456789", result);
-
-		result = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(localDateTime);
-		System.out.println("ISO_LOCAL_DATE: " + result);
-		assertEquals("2023-03-14T09:08:07.123456789", result);
-
-		result = DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(offsetDateTime);
-		System.out.println("ISO_OFFSET_DATE: " + result);
-		assertEquals("2023-03-14T09:08:07.123456789+08:00", result);
-
-		result = DateTimeFormatter.ISO_DATE_TIME.format(localDateTime);
-		System.out.println("ISO_DATE_TIME: " + result);
-		assertEquals("2023-03-14T09:08:07.123456789", result);
-
-		result = DateTimeFormatter.ISO_DATE_TIME.format(zonedDateTime);
-		System.out.println("ISO_DATE_TIME: " + result);
-		assertEquals("2023-03-14T09:08:07.123456789+08:00[Asia/Taipei]", result);
-
-		result = DateTimeFormatter.ISO_ZONED_DATE_TIME.format(zonedDateTime);
-		System.out.println("ISO_ZONED_DATE_TIME: " + result);
-		assertEquals("2023-03-14T09:08:07.123456789+08:00[Asia/Taipei]", result);
-
-		result = DateTimeFormatter.ISO_ORDINAL_DATE.format(zonedDateTime);
-		System.out.println("ISO_ORDINAL_DATE: " + result);
-		assertEquals("2023-073+08:00", result);
-
-		result = DateTimeFormatter.ISO_WEEK_DATE.format(zonedDateTime);
-		System.out.println("ISO_WEEK_DATE: " + result);
-		assertEquals("2023-W11-2+08:00", result);
-
-		result = DateTimeFormatter.ISO_INSTANT.format(zonedDateTime);
-		System.out.println("ISO_INSTANT: " + result);
-		assertEquals("2023-03-14T01:08:07.123456789Z", result);
-
-		result = DateTimeFormatter.RFC_1123_DATE_TIME.format(zonedDateTime);
-		System.out.println("RFC_1123_DATE_TIME: " + result);
-		assertEquals("Tue, 14 Mar 2023 09:08:07 +0800", result);
 	}
 
 	@Test
