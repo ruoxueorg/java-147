f188bdb617c6aecd56605cce8e13df6dfdb0f432
ruoxue <ruoxue777@gmail.com>
2023-07-09 21:02:48 +0800

    update test

--- src/test/java/org/ruoxue/java_147/datetime/offsettime/OffsetTimeClassTest.java
+++ src/test/java/org/ruoxue/java_147/datetime/offsettime/OffsetTimeClassTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import java.time.Duration;
 import java.time.OffsetTime;
-import java.time.OffsetTime;
 import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
 
@@ -14,88 +13,93 @@ public class OffsetTimeClassTest {
 
 	@Test
 	public void plus() {
-		OffsetTime offsetTime = OffsetTime.of(9, 12, 5);
+		ZoneOffset offset = ZoneOffset.ofHours(-5);
+		OffsetTime offsetTime = OffsetTime.of(6, 1, 4, 0, offset);
 		OffsetTime result = offsetTime.plusHours(1);
 		int hour = result.getHour();
 		System.out.println(hour);
-		assertEquals(10, hour);
+		assertEquals(7, hour);
 
 		result = offsetTime.plusMinutes(1);
 		int minute = result.getMinute();
 		System.out.println(minute);
-		assertEquals(13, minute);
+		assertEquals(2, minute);
 
 		result = offsetTime.plusSeconds(1);
 		int second = result.getSecond();
 		System.out.println(second);
-		assertEquals(6, second);
+		assertEquals(5, second);
 	}
 
 	@Test
 	public void plusTemporalAmount() {
-		OffsetTime offsetTime = OffsetTime.of(9, 12, 5);
+		ZoneOffset offset = ZoneOffset.ofHours(-5);
+		OffsetTime offsetTime = OffsetTime.of(6, 1, 4, 0, offset);
 		OffsetTime result = offsetTime.plus(Duration.ofHours(1));
 		int hour = result.getHour();
 		System.out.println(hour);
-		assertEquals(10, hour);
+		assertEquals(7, hour);
 
 		result = offsetTime.plus(Duration.ofMinutes(1));
 		int minute = result.getMinute();
 		System.out.println(minute);
-		assertEquals(13, minute);
+		assertEquals(2, minute);
 
 		result = offsetTime.plus(Duration.ofSeconds(1));
 		int second = result.getSecond();
 		System.out.println(second);
-		assertEquals(6, second);
+		assertEquals(5, second);
 	}
 
 	@Test
 	public void minus() {
-		OffsetTime offsetTime = OffsetTime.of(9, 12, 5);
+		ZoneOffset offset = ZoneOffset.ofHours(-5);
+		OffsetTime offsetTime = OffsetTime.of(6, 1, 4, 0, offset);
 		OffsetTime result = offsetTime.minusHours(1);
 		int hour = result.getHour();
 		System.out.println(hour);
-		assertEquals(8, hour);
+		assertEquals(5, hour);
 
 		result = offsetTime.minusMinutes(1);
 		int minute = result.getMinute();
 		System.out.println(minute);
-		assertEquals(11, minute);
+		assertEquals(0, minute);
 
 		result = offsetTime.minusSeconds(1);
 		int second = result.getSecond();
 		System.out.println(second);
-		assertEquals(4, second);
+		assertEquals(3, second);
 	}
 
 	@Test
 	public void minusTemporalAmount() {
-		OffsetTime offsetTime = OffsetTime.of(9, 12, 5);
+		ZoneOffset offset = ZoneOffset.ofHours(-5);
+		OffsetTime offsetTime = OffsetTime.of(6, 1, 4, 0, offset);
 		OffsetTime result = offsetTime.minus(Duration.ofHours(1));
 		int hour = result.getHour();
 		System.out.println(hour);
-		assertEquals(8, hour);
+		assertEquals(5, hour);
 
 		result = offsetTime.minus(Duration.ofMinutes(1));
 		int minute = result.getMinute();
 		System.out.println(minute);
-		assertEquals(11, minute);
+		assertEquals(0, minute);
 
 		result = offsetTime.minus(Duration.ofSeconds(1));
 		int second = result.getSecond();
 		System.out.println(second);
-		assertEquals(4, second);
+		assertEquals(3, second);
 	}
 
 	@Test
 	public void isBefore() {
-		OffsetTime offsetTime = OffsetTime.of(9, 12, 5);
-		OffsetTime offsetTime2 = OffsetTime.of(10, 12, 5);
+		ZoneOffset offset = ZoneOffset.ofHours(-5);
+		OffsetTime offsetTime = OffsetTime.of(6, 1, 4, 0, offset);
+		OffsetTime offsetTime2 = OffsetTime.of(7, 1, 4, 0, offset);
 		boolean result = offsetTime.isBefore(offsetTime2);
 		System.out.println(result);
 		assertTrue(result);
-		
+
 		result = offsetTime2.isBefore(offsetTime);
 		System.out.println(result);
 		assertFalse(result);
@@ -103,12 +107,13 @@ public class OffsetTimeClassTest {
 
 	@Test
 	public void isAfter() {
-		OffsetTime offsetTime = OffsetTime.of(9, 12, 5);
-		OffsetTime offsetTime2 = OffsetTime.of(8, 12, 5);
+		ZoneOffset offset = ZoneOffset.ofHours(-5);
+		OffsetTime offsetTime = OffsetTime.of(6, 1, 4, 0, offset);
+		OffsetTime offsetTime2 = OffsetTime.of(5, 0, 3, 0, offset);
 		boolean result = offsetTime.isAfter(offsetTime2);
 		System.out.println(result);
 		assertTrue(result);
-		
+
 		result = offsetTime2.isAfter(offsetTime);
 		System.out.println(result);
 		assertFalse(result);
@@ -116,8 +121,9 @@ public class OffsetTimeClassTest {
 
 	@Test
 	public void until() {
-		OffsetTime offsetTime = OffsetTime.of(9, 12, 5);
-		OffsetTime offsetTime2 = OffsetTime.of(10, 12, 5);
+		ZoneOffset offset = ZoneOffset.ofHours(-5);
+		OffsetTime offsetTime = OffsetTime.of(6, 1, 4, 0, offset);
+		OffsetTime offsetTime2 = OffsetTime.of(7, 1, 4, 0, offset);
 		long result = offsetTime.until(offsetTime2, ChronoUnit.HOURS);
 		System.out.println(result);
 		assertEquals(1, result);
@@ -129,18 +135,5 @@ public class OffsetTimeClassTest {
 		result = offsetTime.until(offsetTime2, ChronoUnit.SECONDS);
 		System.out.println(result);
 		assertEquals(3600, result);
-	}
-
-	@Test
-	public void atOffset() {
-		OffsetTime offsetTime = OffsetTime.of(9, 12, 5);
-		OffsetTime result = offsetTime.atOffset(ZoneOffset.of("+01:00"));
-		System.out.println(result);
-		assertEquals("09:12:05+01:00", result.toString());
-
-		offsetTime = OffsetTime.of(9, 12, 5, 123456789);
-		result = offsetTime.atOffset(ZoneOffset.ofHours(1));
-		System.out.println(result);
-		assertEquals("09:12:05.123456789+01:00", result.toString());
 	}
 }
