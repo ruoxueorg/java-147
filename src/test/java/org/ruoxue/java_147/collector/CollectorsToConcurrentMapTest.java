6d6b138827bc8cf31a491a25295e4cefb62259ca
ruoxue <ruoxue777@gmail.com>
2023-08-02 20:05:40 +0800

    update test

--- src/test/java/org/ruoxue/java_147/collector/CollectorsToConcurrentMapTest.java
+++ src/test/java/org/ruoxue/java_147/collector/CollectorsToConcurrentMapTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -14,49 +14,49 @@ import org.junit.Test;
 public class CollectorsToConcurrentMapTest {
 
 	@Test
-	public void toConcurrentMap() {
+	public void toMap() {
 		int expectedSize = 3;
 		List<String> list = Arrays.asList("Blueberry", "Melon", "Fig");
 		Function<String, String> key = s -> s.toUpperCase();
 		Function<String, Integer> length = s -> s.length();
-		Map<String, Integer> result = list.stream().collect(Collectors.toConcurrentMap(key, length));
+		Map<String, Integer> result = list.stream().collect(Collectors.toMap(key, length));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 
-		result = list.stream().collect(Collectors.toConcurrentMap(Function.identity(), String::length));
+		result = list.stream().collect(Collectors.toMap(Function.identity(), String::length));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 
 		Function<Integer, Integer> twice = i -> i * i;
-		result = list.stream().collect(Collectors.toConcurrentMap(key, length.andThen(twice)));
+		result = list.stream().collect(Collectors.toMap(key, length.andThen(twice)));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 	}
 
 	@Test(expected = IllegalStateException.class)
-	public void toConcurrentMapThrowException() {
+	public void toMapThrowException() {
 		int expectedSize = 3;
 		List<String> list = Arrays.asList("Blueberry", "Melon", "Fig", "Blueberry", "Melon");
 		Function<String, String> key = s -> s.toUpperCase();
 		Function<String, Integer> length = s -> s.length();
-		Map<String, Integer> result = list.stream().collect(Collectors.toConcurrentMap(key, length));
+		Map<String, Integer> result = list.stream().collect(Collectors.toMap(key, length));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 	}
 
 	@Test
-	public void toConcurrentMapWithDuplicateKey() {
+	public void toMapWithDuplicateKey() {
 		int expectedSize = 3;
 		List<String> list = Arrays.asList("Blueberry", "Melon", "Fig", "Blueberry", "Melon");
 		Function<String, String> key = s -> s.toUpperCase();
 		Function<String, Integer> length = s -> s.length();
 		Map<String, Integer> result = list.stream()
-				.collect(Collectors.toConcurrentMap(key, length, (oldValue, newValue) -> oldValue));
+				.collect(Collectors.toMap(key, length, (oldValue, newValue) -> oldValue));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 
 		result = list.stream()
-				.collect(Collectors.toConcurrentMap(Function.identity(), String::length, (oldValue, newValue) -> oldValue));
+				.collect(Collectors.toMap(Function.identity(), String::length, (oldValue, newValue) -> oldValue));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 	}
@@ -68,10 +68,9 @@ public class CollectorsToConcurrentMapTest {
 		Function<String, String> key = s -> s.toLowerCase();
 		Function<String, Integer> length = s -> s.length();
 		Function<Integer, Integer> twice = i -> i * i;
-		Map<String, Integer> result = list.stream()
-				.collect(Collectors.toMap(key, length.andThen(twice), (oldValue, newValue) -> oldValue, TreeMap::new));
+		Map<String, Integer> result = list.stream().collect(Collectors.toConcurrentMap(key, length.andThen(twice),
+				(oldValue, newValue) -> oldValue, ConcurrentHashMap::new));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 	}
-
 }
--- src/test/java/org/ruoxue/java_147/collector/CollectorsToMapTest.java
+++ src/test/java/org/ruoxue/java_147/collector/CollectorsToMapTest.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -15,49 +14,49 @@ import org.junit.Test;
 public class CollectorsToMapTest {
 
 	@Test
-	public void toMap() {
+	public void toConcurrentMap() {
 		int expectedSize = 3;
 		List<String> list = Arrays.asList("Blueberry", "Melon", "Fig");
 		Function<String, String> key = s -> s.toUpperCase();
 		Function<String, Integer> length = s -> s.length();
-		Map<String, Integer> result = list.stream().collect(Collectors.toMap(key, length));
+		Map<String, Integer> result = list.stream().collect(Collectors.toConcurrentMap(key, length));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 
-		result = list.stream().collect(Collectors.toMap(Function.identity(), String::length));
+		result = list.stream().collect(Collectors.toConcurrentMap(Function.identity(), String::length));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 
 		Function<Integer, Integer> twice = i -> i * i;
-		result = list.stream().collect(Collectors.toMap(key, length.andThen(twice)));
+		result = list.stream().collect(Collectors.toConcurrentMap(key, length.andThen(twice)));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 	}
 
 	@Test(expected = IllegalStateException.class)
-	public void toMapThrowException() {
+	public void toConcurrentMapThrowException() {
 		int expectedSize = 3;
 		List<String> list = Arrays.asList("Blueberry", "Melon", "Fig", "Blueberry", "Melon");
 		Function<String, String> key = s -> s.toUpperCase();
 		Function<String, Integer> length = s -> s.length();
-		Map<String, Integer> result = list.stream().collect(Collectors.toMap(key, length));
+		Map<String, Integer> result = list.stream().collect(Collectors.toConcurrentMap(key, length));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 	}
 
 	@Test
-	public void toMapWithDuplicateKey() {
+	public void toConcurrentMapWithDuplicateKey() {
 		int expectedSize = 3;
 		List<String> list = Arrays.asList("Blueberry", "Melon", "Fig", "Blueberry", "Melon");
 		Function<String, String> key = s -> s.toUpperCase();
 		Function<String, Integer> length = s -> s.length();
 		Map<String, Integer> result = list.stream()
-				.collect(Collectors.toMap(key, length, (oldValue, newValue) -> oldValue));
+				.collect(Collectors.toConcurrentMap(key, length, (oldValue, newValue) -> oldValue));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 
 		result = list.stream()
-				.collect(Collectors.toMap(Function.identity(), String::length, (oldValue, newValue) -> oldValue));
+				.collect(Collectors.toConcurrentMap(Function.identity(), String::length, (oldValue, newValue) -> oldValue));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 	}
@@ -70,8 +69,9 @@ public class CollectorsToMapTest {
 		Function<String, Integer> length = s -> s.length();
 		Function<Integer, Integer> twice = i -> i * i;
 		Map<String, Integer> result = list.stream()
-				.collect(Collectors.toConcurrentMap(key, length.andThen(twice), (oldValue, newValue) -> oldValue, ConcurrentMap::new));
+				.collect(Collectors.toMap(key, length.andThen(twice), (oldValue, newValue) -> oldValue, TreeMap::new));
 		System.out.println(result);
 		assertEquals(expectedSize, result.size());
 	}
+
 }
