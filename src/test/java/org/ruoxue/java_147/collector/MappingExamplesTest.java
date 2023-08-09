54083af8ff74553fb179558ef3328cdf2f8d676b
ruoxue <ruoxue777@gmail.com>
2023-08-10 00:38:40 +0800

    add test

--- src/test/java/org/ruoxue/java_147/collector/MappingExamplesTest.java
+++ src/test/java/org/ruoxue/java_147/collector/MappingExamplesTest.java
@@ -3,9 +3,10 @@ package org.ruoxue.java_147.collector;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -17,7 +18,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-public class CollectorsMappingTest {
+public class MappingExamplesTest {
 
 	@NoArgsConstructor
 	@Getter
@@ -45,42 +46,42 @@ public class CollectorsMappingTest {
 	}
 
 	@Test
-	public void mappingWithMaxBy() {
+	public void mappingWithCounting() {
 		List<Fruit> list = Arrays.asList(new Fruit("Blueberry", Double.MAX_VALUE, 1), new Fruit("Melon", -1, 3),
 				new Fruit("Fig", 3, 1));
-		Optional<Double> result = list.stream()
-				.collect(Collectors.mapping(Fruit::getQuantity, Collectors.maxBy(Double::compareTo)));
-		System.out.println(result);
-		assertEquals(Double.MAX_VALUE, result.get(), 0);
-	}
+		Map<Integer, Long> result = list.stream().collect(
+				Collectors.groupingBy(Fruit::getType, Collectors.mapping(Function.identity(), Collectors.counting())));
 
-	@Test
-	public void mappingWithMinBy() {
-		List<Fruit> list = Arrays.asList(new Fruit("Blueberry", Double.MAX_VALUE, 1), new Fruit("Melon", -1, 3),
-				new Fruit("Fig", 3, 1));
-		Optional<Integer> result = list.stream()
-				.collect(Collectors.mapping(e -> e.name.length(), Collectors.minBy(Integer::compareTo)));
-		System.out.println(result);
-		assertEquals(3, result.get().intValue());
-	}
-
-	@Test
-	public void mappingWithGroupingBy() {
-		List<Fruit> list = Arrays.asList(new Fruit("Blueberry", Double.MAX_VALUE, 1), new Fruit("Melon", -1, 3),
-				new Fruit("Fig", 3, 1));
-		Map<Integer, List<Double>> result = list.stream().collect(
-				Collectors.groupingBy(Fruit::getType, Collectors.mapping(Fruit::getQuantity, Collectors.toList())));
 		System.out.println(result);
 		assertEquals(2, result.size());
 	}
 
 	@Test
-	public void mappingWithPartitioningBy() {
+	public void mappingWithJoining() {
 		List<Fruit> list = Arrays.asList(new Fruit("Blueberry", Double.MAX_VALUE, 1), new Fruit("Melon", -1, 3),
 				new Fruit("Fig", 3, 1));
-		Map<Boolean, List<Double>> result = list.stream().collect(Collectors.partitioningBy(e -> e.name.length() > 5,
-				Collectors.mapping(Fruit::getQuantity, Collectors.toList())));
+		String result = list.stream().collect(Collectors.mapping(Fruit::getName, Collectors.joining(", ", "(", ")")));
 		System.out.println(result);
-		assertEquals(2, result.size());
+		assertEquals("(Blueberry, Melon, Fig)", result);
+	}
+
+	@Test
+	public void mappingWithSummarizingInt() {
+		List<Fruit> list = Arrays.asList(new Fruit("Blueberry", Double.MAX_VALUE, 1), new Fruit("Melon", -1, 3),
+				new Fruit("Fig", 3, 1));
+		IntSummaryStatistics result = list.stream()
+				.collect(Collectors.mapping(e -> e.name.length(), Collectors.summarizingInt(e -> e)));
+		System.out.println(result);
+		assertEquals(5.66, result.getAverage(), 2);
+		assertEquals(17, result.getSum());
+	}
+
+	@Test
+	public void mappingWithToList() {
+		List<Fruit> list = Arrays.asList(new Fruit("Blueberry", Double.MAX_VALUE, 1), new Fruit("Melon", -1, 3),
+				new Fruit("Fig", 3, 1));
+		List<Double> result = list.stream().collect(Collectors.mapping(Fruit::getQuantity, Collectors.toList()));
+		System.out.println(result);
+		assertEquals(3, result.size());
 	}
 }
