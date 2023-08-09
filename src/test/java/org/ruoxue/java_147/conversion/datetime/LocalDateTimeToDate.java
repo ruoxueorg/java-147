c66bcc0049cc6dbde9090c99bc708861e9b6ff95
ruoxue <ruoxue777@gmail.com>
2023-08-09 16:28:42 +0800

    rename toLocalDateWithValueOf

--- src/test/java/org/ruoxue/java_147/conversion/datetime/LocalDateTimeToDate.java
+++ src/test/java/org/ruoxue/java_147/conversion/datetime/LocalDateTimeToDate.java
@@ -32,15 +32,4 @@ public class LocalDateTimeToDate {
 		System.out.println(date);
 		assertEquals("Mon Apr 03 21:00:00 CST 2023", date.toString());
 	}
-
-	@Test
-	public void toDateWithZonedDateTime() {
-		ZoneId zone = ZoneId.of("Asia/Sakhalin");
-		LocalDateTime localDateTime = LocalDateTime.of(2023, 4, 4, 5, 4, 3);
-
-		ZonedDateTime zonedDateTime = localDateTime.atZone(zone);
-		Date date = Date.from(zonedDateTime.toInstant());
-		System.out.println(date);
-		assertEquals("Tue Apr 04 02:04:03 CST 2023", date.toString());
-	}
 }
