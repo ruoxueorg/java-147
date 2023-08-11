c13592d0539f20c03cc4054365cd64cbe1fc9261
ruoxue <ruoxue777@gmail.com>
2023-08-11 18:33:15 +0800

    update withCounting

--- src/test/java/org/ruoxue/java_147/collector/PartitioningByExamplesTest.java
+++ src/test/java/org/ruoxue/java_147/collector/PartitioningByExamplesTest.java
@@ -5,11 +5,13 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.Test;
+import org.ruoxue.java_147.collector.MappingExamplesTest.Fruit;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -72,6 +74,23 @@ public class PartitioningByExamplesTest {
 				new Fruit("Fig", 3, 1), new Fruit("Guava", 4, 2), new Fruit("Fig", 5, 3));
 		Map<Boolean, List<Double>> fruitResult = fruitList.stream().collect(Collectors.partitioningBy(e -> e.type > 1,
 				Collectors.mapping(Fruit::getQuantity, Collectors.toList())));
+		System.out.println(fruitResult);
+		assertEquals(2, fruitResult.size());
+	}
+
+	@Test
+	public void withCounting() {
+		List<String> list = Arrays.asList("Blueberry", "Melon", "Fig", "Guava", "Kiwifruit");
+		Map<Boolean, Long> result = list.stream()
+				.collect(Collectors.partitioningBy(e -> e.length() > 3, Collectors.counting()));
+		System.out.println(result);
+		assertEquals(2, result.size());
+
+		List<Fruit> fruitList = Arrays.asList(new Fruit("Blueberry", Double.MAX_VALUE, 1), new Fruit("Melon", -1, 3),
+				new Fruit("Fig", 3, 1));
+		Map<Boolean, Long> fruitResult = fruitList.stream()
+				.collect(Collectors.partitioningBy(e -> e.type > 1, Collectors.counting()));
+
 		System.out.println(fruitResult);
 		assertEquals(2, fruitResult.size());
 	}
