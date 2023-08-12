c5c0b631ff30bdb8c0714d0cb768b0632964e4dc
ruoxue <ruoxue777@gmail.com>
2023-08-13 01:00:18 +0800

    update test

--- src/test/java/org/ruoxue/java_147/string/StringClassTest.java
+++ src/test/java/org/ruoxue/java_147/string/StringClassTest.java
@@ -10,85 +10,84 @@ import org.junit.Test;
 
 public class StringClassTest {
 
-	@Test
-	public void charAt() {
-		char expected = 'g';
-		StringBuffer builder = new StringBuffer();
-		builder.append("Spring boot 168");
-		char result = builder.charAt(5);
-		System.out.println(result);
-		assertEquals(expected, result);
-
-		result = builder.charAt(7);
-		System.out.println(result);
-		assertEquals('b', result);
-	}
-
-	@Test
-	public void getChars() {
-		StringBuffer builder = new StringBuffer();
-		builder.append("Spring boot 168");
-		IntStream stream = builder.chars();
-		List<Integer> list = stream.boxed().collect(Collectors.toList());
-		System.out.println(list);
-	}
-
-	@Test
-	public void indexOf() {
-		int expected = 3;
-		StringBuffer builder = new StringBuffer();
-		builder.append("Spring boot 168");
-		int result = builder.indexOf("i");
-		System.out.println(result);
-		assertEquals(expected, result);
-
-		result = builder.indexOf("i", 2);
-		System.out.println(result);
-		assertEquals(3, result);
-	}
-
-	@Test
-	public void lastIndexOf() {
-		int expected = 3;
-		StringBuffer builder = new StringBuffer();
-		builder.append("Spring boot 168");
-		int result = builder.lastIndexOf("i");
-		System.out.println(result);
-		assertEquals(expected, result);
-
-		result = builder.lastIndexOf("i", 5);
-		System.out.println(result);
-		assertEquals(3, result);
-	}
-
-	@Test
-	public void setCharAt() {
-		StringBuffer builder = new StringBuffer();
-		builder.append("Spring boot 168");
-		builder.setCharAt(3, 'I');
-		System.out.println(builder);
-		assertEquals("SprIng boot 168", builder.toString());
-	}
+//	@Test
+//	public void charAt() {
+//		char expected = 'g';
+//		StringBuffer builder = new StringBuffer();
+//		builder.append("Spring boot 168");
+//		char result = builder.charAt(5);
+//		System.out.println(result);
+//		assertEquals(expected, result);
+//
+//		result = builder.charAt(7);
+//		System.out.println(result);
+//		assertEquals('b', result);
+//	}
+//
+//	@Test
+//	public void getChars() {
+//		StringBuffer builder = new StringBuffer();
+//		builder.append("Spring boot 168");
+//		IntStream stream = builder.chars();
+//		List<Integer> list = stream.boxed().collect(Collectors.toList());
+//		System.out.println(list);
+//	}
+//
+//	@Test
+//	public void indexOf() {
+//		int expected = 3;
+//		StringBuffer builder = new StringBuffer();
+//		builder.append("Spring boot 168");
+//		int result = builder.indexOf("i");
+//		System.out.println(result);
+//		assertEquals(expected, result);
+//
+//		result = builder.indexOf("i", 2);
+//		System.out.println(result);
+//		assertEquals(3, result);
+//	}
+//
+//	@Test
+//	public void lastIndexOf() {
+//		int expected = 3;
+//		StringBuffer builder = new StringBuffer();
+//		builder.append("Spring boot 168");
+//		int result = builder.lastIndexOf("i");
+//		System.out.println(result);
+//		assertEquals(expected, result);
+//
+//		result = builder.lastIndexOf("i", 5);
+//		System.out.println(result);
+//		assertEquals(3, result);
+//	}
+//
+//	@Test
+//	public void setCharAt() {
+//		StringBuffer builder = new StringBuffer();
+//		builder.append("Spring boot 168");
+//		builder.setCharAt(3, 'I');
+//		System.out.println(builder);
+//		assertEquals("SprIng boot 168", builder.toString());
+//	}
 
 	@Test
 	public void substring() {
-		StringBuffer builder = new StringBuffer();
-		builder.append("Spring boot 168");
-		String result = builder.substring(5);
+		String value = "Assertj 155";
+		String result = value.substring(8);
 		System.out.println(result);
-		assertEquals("g boot 168", result);
+		assertEquals("155", result);
 
-		result = builder.substring(0, 5);
+		result = value.substring(0, 7);
 		System.out.println(result);
-		assertEquals("Sprin", result);
+		assertEquals("Assertj", result);
 	}
 
-	@Test
-	public void replace() {
-		StringBuffer builder = new StringBuffer();
-		builder.append("Spring boot 168");
-		builder.replace(0, 4, "SPRING");
-		System.out.println(builder);
-		assertEquals("SPRINGng boot 168", builder.toString());
-	}
+//	@Test
+//	public void replace() {
+//		StringBuffer builder = new StringBuffer();
+//		builder.append("Spring boot 168");
+//		builder.replace(0, 4, "SPRING");
+//		System.out.println(builder);
+//		assertEquals("SPRINGng boot 168", builder.toString());
+//	}
 }
--- src/test/java/org/ruoxue/java_147/string/StringMethodsTest.java
+++ src/test/java/org/ruoxue/java_147/string/StringMethodsTest.java
@@ -6,80 +6,86 @@ import org.junit.Test;
 
 public class StringMethodsTest {
 
-	@Test
-	public void append() {
-		int expectedSize = 26;
-		StringBuffer builder = new StringBuffer();
-		builder.append("Sprin");
-		builder.append('g');
-		builder.append(" ");
-		builder.append("boot");
-		builder.append(" ");
-		builder.append(168);
-		builder.append(", ");
-		builder.append(new StringBuffer().append("JUnit 151"));
-		System.out.println(builder);
-		assertEquals(expectedSize, builder.length());
-	}
+//	@Test
+//	public void append() {
+//		int expectedSize = 26;
+//		StringBuffer builder = new StringBuffer();
+//		builder.append("Sprin");
+//		builder.append('g');
+//		builder.append(" ");
+//		builder.append("boot");
+//		builder.append(" ");
+//		builder.append(168);
+//		builder.append(", ");
+//		builder.append(new StringBuffer().append("JUnit 151"));
+//		System.out.println(builder);
+//		assertEquals(expectedSize, builder.length());
+//	}
 
-	@Test
-	public void insert() {
-		int expectedSize = 24;
-		StringBuffer builder = new StringBuffer();
-		builder.append("Spring boot 168");
-		builder.insert(12, "JUnit 151");
-		System.out.println(builder);
-		assertEquals(expectedSize, builder.length());
-	}
+//	@Test
+//	public void insert() {
+//		int expectedSize = 24;
+//		StringBuffer builder = new StringBuffer();
+//		builder.append("Spring boot 168");
+//		builder.insert(12, "JUnit 151");
+//		System.out.println(builder);
+//		assertEquals(expectedSize, builder.length());
+//	}
 
-	@Test
-	public void delete() {
-		int expectedSize = 8;
-		StringBuffer builder = new StringBuffer("Spring boot 168");
-		builder.delete(0, 7);
-		System.out.println(builder);
-		assertEquals(expectedSize, builder.length());
-	}
-
-	@Test
-	public void deleteCharAt() {
-		int expectedSize = 14;
-		StringBuffer builder = new StringBuffer("Spring boot 168");
-		builder.deleteCharAt(12);
-		System.out.println(builder);
-		assertEquals(expectedSize, builder.length());
-	}
+//	@Test
+//	public void delete() {
+//		int expectedSize = 8;
+//		StringBuffer builder = new StringBuffer("Spring boot 168");
+//		builder.delete(0, 7);
+//		System.out.println(builder);
+//		assertEquals(expectedSize, builder.length());
+//	}
+//
+//	@Test
+//	public void deleteCharAt() {
+//		int expectedSize = 14;
+//		StringBuffer builder = new StringBuffer("Spring boot 168");
+//		builder.deleteCharAt(12);
+//		System.out.println(builder);
+//		assertEquals(expectedSize, builder.length());
+//	}
 
 	@Test
 	public void length() {
-		int expectedSize = 36;
-		StringBuffer builder = new StringBuffer();
-		builder.append("Java 147, ");
-		builder.append("Spring boot 168, ");
-		builder.append("JUnit 151");
-		System.out.println(builder.length());
-		assertEquals(expectedSize, builder.length());
+		int expectedSize = 11;
+		String value = "Assertj 155";
+		System.out.println(value.length());
+		assertEquals(expectedSize, value.length());
 	}
 
 	@Test
-	public void capacity() {
-		int expectedSize = 34;
-		StringBuffer builder = new StringBuffer();
-		builder.append("Spring boot 168, ");
-		builder.append("JUnit 151");
-		System.out.println(builder.capacity());
-		assertEquals(expectedSize, builder.capacity());
+	public void format() {
+		String value = "Assertj 155";
+		String formatted = String.format("String is: %s", value);
+		System.out.println(formatted);
+		formatted = String.format("String is: %15s", value);
+		System.out.println(formatted);
 	}
 
-	@Test
-	public void trimToSize() {
-		int expectedSize = 26;
-		StringBuffer builder = new StringBuffer();
-		builder.append("Spring boot 168, ");
-		builder.append("JUnit 151");
-		System.out.println(builder.capacity());
-		builder.trimToSize();
-		System.out.println(builder.capacity());
-		assertEquals(expectedSize, builder.capacity());
-	}
+//	@Test
+//	public void capacity() {
+//		int expectedSize = 34;
+//		StringBuffer builder = new StringBuffer();
+//		builder.append("Spring boot 168, ");
+//		builder.append("JUnit 151");
+//		System.out.println(builder.capacity());
+//		assertEquals(expectedSize, builder.capacity());
+//	}
+//
+//	@Test
+//	public void trimToSize() {
+//		int expectedSize = 26;
+//		StringBuffer builder = new StringBuffer();
+//		builder.append("Spring boot 168, ");
+//		builder.append("JUnit 151");
+//		System.out.println(builder.capacity());
+//		builder.trimToSize();
+//		System.out.println(builder.capacity());
+//		assertEquals(expectedSize, builder.capacity());
+//	}
 }
