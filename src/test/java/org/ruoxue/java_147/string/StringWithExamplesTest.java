b46d1ec91fdda3b8badd80bf8d50895577c7a2bb
ruoxue <ruoxue777@gmail.com>
2023-08-13 13:36:19 +0800

    update test

--- src/test/java/org/ruoxue/java_147/string/StringWithExamplesTest.java
+++ src/test/java/org/ruoxue/java_147/string/StringWithExamplesTest.java
@@ -2,44 +2,40 @@ package org.ruoxue.java_147.string;
 
 import static org.junit.Assert.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 import org.junit.Test;
 
 public class StringWithExamplesTest {
 
-//	@Test
-//	public void split() {
-//		int expectedSize = 17;
-//		StringBuffer builder = new StringBuffer();
-//		builder.append("Spring boot 168");
-//		builder.appendCodePoint(32);
-//		builder.appendCodePoint(65);
-//		System.out.println(builder);
-//		assertEquals(expectedSize, builder.length());
-//	}
-//
-//	@Test
-//	public void compareTo() {
-//		StringBuffer builder = new StringBuffer();
-//		builder.append("Spring boot 168");
-//		IntStream stream = builder.codePoints();
-//		List<Integer> list = stream.boxed().collect(Collectors.toList());
-//		System.out.println(list);
-//	}
-//
-//	@Test
-//	public void strip() {
-//		int expected = 103;
-//		StringBuffer builder = new StringBuffer();
-//		builder.append("Spring boot 168");
-//		int result = builder.codePointAt(5);
-//		System.out.println(result);
-//		assertEquals(expected, result);
-//	}
-//
+	@Test
+	public void split() {
+		String value = "Assertj 155";
+		String[] result = value.split(" ");
+		System.out.println(Arrays.toString(result));
+		assertEquals(2, result.length);
+
+		result = value.split(",");
+		System.out.println(Arrays.toString(result));
+		assertEquals(1, result.length);
+	}
+
+	@Test
+	public void compareTo() {
+		String value = "Assertj 155";
+		int result = value.compareTo("155");
+		System.out.println(result);
+		assertTrue(result > 0);
+
+		result = value.compareTo("assertj");
+		System.out.println(result);
+		assertTrue(result < 0);
+
+		result = value.compareTo("Assertj 155");
+		System.out.println(result);
+		assertTrue(result == 0);
+	}
+
 //	@Test
 //	public void valueOf() {
 //		int expected = 110;
@@ -69,4 +65,9 @@ public class StringWithExamplesTest {
 //		System.out.println(result);
 //		assertEquals(expected, result);
 //	}
+
+	@Test
+	public void intern() {
+
+	}
 }
