c6d5f1c08054a1c84ed81f282c8c76864acbc4c6
ruoxue <ruoxue777@gmail.com>
2023-09-25 23:15:55 +0800

    update query

--- src/test/java/org/ruoxue/java_147/datetime/instant/InstantWithExamplesTest.java
+++ src/test/java/org/ruoxue/java_147/datetime/instant/InstantWithExamplesTest.java
@@ -2,11 +2,18 @@ package org.ruoxue.java_147.datetime.instant;
 
 import static org.junit.Assert.*;
 
+import java.time.Clock;
 import java.time.Instant;
+import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.Chronology;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalQueries;
+import java.time.temporal.ValueRange;
 
 import org.junit.Test;
 
@@ -68,6 +75,22 @@ public class InstantWithExamplesTest {
 	@Test
 	public void compareTo() {
 		Instant instant = Instant.parse("2023-09-12T04:05:06.123456789Z");
+		Instant instant2 = Instant.parse("2024-09-12T04:05:06.123456789Z");
+		int result = instant.compareTo(instant2);
+		System.out.println(result);
+		assertTrue(result < 0);
+
+		instant = Instant.parse("2023-09-12T04:05:06.123456789Z");
+		instant2 = Instant.parse("2023-09-12T04:05:06.123456789Z");
+		result = instant.compareTo(instant2);
+		System.out.println(result);
+		assertTrue(result == 0);
+
+		instant = Instant.parse("2024-09-12T04:05:06.123456789Z");
+		instant2 = Instant.parse("2023-09-12T04:05:06.123456789Z");
+		result = instant.compareTo(instant2);
+		System.out.println(result);
+		assertTrue(result > 0);
 	}
 
 	@Test
@@ -77,13 +100,31 @@ public class InstantWithExamplesTest {
 		System.out.println(result);
 		assertEquals("Nanos", result);
 
-		result = instant.query(TemporalQueries.zoneId()).toString();
-		System.out.println(result);
-		assertEquals("Nanos", result);
+		instant = Instant.now();
+		ZoneOffset offset = ZoneOffset.ofHours(-4);
+		OffsetDateTime offsetDateTime = instant.atOffset(offset);
+		System.out.println(offsetDateTime);
+		ZoneOffset zoneOffset = offsetDateTime.query(TemporalQueries.offset());
+		System.out.println(zoneOffset);
+		assertEquals("-04:00", zoneOffset.toString());
+
+		ZoneId zone = ZoneId.of("America/New_York");
+		ZonedDateTime zonedDateTime = instant.atZone(zone);
+		System.out.println(zonedDateTime);
+		ZoneId zoneId = zonedDateTime.query(TemporalQueries.zoneId());
+		System.out.println(zoneId);
+		assertEquals("America/New_York", zoneId.toString());
 	}
 
 	@Test
 	public void range() {
 		Instant instant = Instant.parse("2023-09-12T04:05:06.123456789Z");
+		ValueRange result = instant.range(ChronoField.MILLI_OF_SECOND);
+		System.out.println(result);
+		assertEquals("0 - 999", result.toString());
+
+		instant.range(ChronoField.NANO_OF_SECOND);
+		System.out.println(result);
+		assertEquals("0 - 999", result.toString());
 	}
 }
