e6850a9d5a12eccb1e1fa03c7e88d381afd0b380
ruoxue <ruoxue777@gmail.com>
2023-10-04 15:47:25 +0800

    update test

--- src/test/java/org/ruoxue/java_147/collection/CollectionsClassTest.java
+++ src/test/java/org/ruoxue/java_147/collection/CollectionsClassTest.java
@@ -1,9 +1,8 @@
 package org.ruoxue.java_147.collection;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -12,6 +11,21 @@ import org.junit.Test;
 
 public class CollectionsClassTest {
 
+	@Test
+	public void sort() {
+		List<String> list = Arrays.asList("Orange", "Mango", "Peach");
+		System.out.println(list);
+		Collections.sort(list);
+		System.out.println(list);
+		assertThat(list).containsExactly("Mango", "Orange", "Peach");
+
+		List<Integer> intList = Arrays.asList(Integer.MAX_VALUE, -1, 3);
+		System.out.println(intList);
+		Collections.sort(intList);
+		System.out.println(intList);
+		assertThat(intList).containsExactly(-1, 3, Integer.MAX_VALUE);
+	}
+
 	public static Comparator<String> nameComparator = new Comparator<String>() {
 		@Override
 		public int compare(String o1, String o2) {
@@ -19,70 +33,70 @@ public class CollectionsClassTest {
 		}
 	};
 
-	@Test
-	public void sort() {
-		List<String> list = new ArrayList<String>();
-		list.add("Mango");
-		list.add("Peach");
-		list.add("Orange");
-		System.out.println(list);
-
-		Collections.sort(list);
-		System.out.println(list);
-	}
+	protected static Comparator<Integer> valueComparator = new Comparator<Integer>() {
+		@Override
+		public int compare(Integer i1, Integer i2) {
+			return Integer.compare(i1, i2);
+		}
+	};
 
 	@Test
 	public void sortWithComparator() {
-		List<String> list = new ArrayList<String>();
-		list.add("Mango");
-		list.add("Peach");
-		list.add("Orange");
+		List<String> list = Arrays.asList("Orange", "Mango", "Peach");
 		System.out.println(list);
-
 		Collections.sort(list, nameComparator);
 		System.out.println(list);
+		assertThat(list).containsExactly("Mango", "Orange", "Peach");
+
+		List<Integer> intList = Arrays.asList(Integer.MAX_VALUE, 3, -1);
+		System.out.println(intList);
+		Collections.sort(intList, valueComparator);
+		System.out.println(intList);
+		assertThat(intList).containsExactly(-1, 3, Integer.MAX_VALUE);
 	}
 
 	@Test
 	public void sortWithLambda() {
-		List<String> list = new ArrayList<String>();
-		list.add("Mango");
-		list.add("Peach");
-		list.add("Orange");
+		List<String> list = Arrays.asList("Orange", "Mango", "Peach");
 		System.out.println(list);
-
 		Collections.sort(list, (o1, o2) -> o1.compareTo(o2));
 		System.out.println(list);
+		assertThat(list).containsExactly("Mango", "Orange", "Peach");
 
-		Collections.sort(list, (o1, o2) -> {
-			return o2.compareTo(o1);
-		});
-		System.out.println(list);
+		List<Integer> intList = Arrays.asList(Integer.MAX_VALUE, -1, 3);
+		System.out.println(intList);
+		Collections.sort(intList, (i1, i2) -> Integer.compare(i1, i2));
+		System.out.println(intList);
+		assertThat(intList).containsExactly(-1, 3, Integer.MAX_VALUE);
 	}
 
 	@Test
 	public void max() {
-		String expected = "Peach";
-		List<String> list = new ArrayList<String>();
-		list.add("Mango");
-		list.add("Orange");
-		list.add("Peach");
+		List<String> list = Arrays.asList("Orange", "Mango", "Peach");
 		System.out.println(list);
-		String value = Collections.max(list);
-		System.out.println(value);
-		assertEquals(expected, value);
+		String result = Collections.max(list);
+		System.out.println(result);
+		assertThat(result).isEqualTo("Peach");
+
+		List<Integer> intList = Arrays.asList(Integer.MAX_VALUE, 3, -1);
+		System.out.println(intList);
+		int intResult = Collections.max(intList);
+		System.out.println(intResult);
+		assertThat(intResult).isEqualTo(Integer.MAX_VALUE);
 	}
 
 	@Test
 	public void min() {
-		String expected = "Mango";
-		List<String> list = new ArrayList<String>();
-		list.add("Mango");
-		list.add("Orange");
-		list.add("Peach");
+		List<String> list = Arrays.asList("Orange", "Mango", "Peach");
 		System.out.println(list);
-		String value = Collections.min(list);
-		System.out.println(value);
-		assertEquals(expected, value);
+		String result = Collections.min(list);
+		System.out.println(result);
+		assertThat(result).isEqualTo("Mango");
+
+		List<Integer> intList = Arrays.asList(Integer.MAX_VALUE, 3, -1);
+		System.out.println(intList);
+		int intResult = Collections.min(intList);
+		System.out.println(intResult);
+		assertThat(intResult).isEqualTo(-1);
 	}
 }
