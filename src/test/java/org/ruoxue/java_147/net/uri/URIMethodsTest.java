35747f9fbdc3e017a1bc56029dd6f44c48d84232
ruoxue <ruoxue777@gmail.com>
2023-11-04 20:25:37 +0800

    add compareTo

--- src/test/java/org/ruoxue/java_147/net/uri/URIMethodsTest.java
+++ src/test/java/org/ruoxue/java_147/net/uri/URIMethodsTest.java
@@ -186,4 +186,53 @@ public class URIMethodsTest {
 			System.out.println(url);
 		}).isInstanceOf(MalformedURLException.class);
 	}
+
+	@Test
+	public void compareTo() {
+		URI uri = URI.create("https://www.ruoxue.org");
+		System.out.println(uri);
+		URI uri2 = URI.create("https://WWW.RUOXUE.ORG");
+		System.out.println(uri2);
+		int result = uri.compareTo(uri2);
+		System.out.println(result);
+		assertThat(result).isZero();
+
+		uri = URI.create("https://www.ruoxue.org?s=a");
+		System.out.println(uri);
+		uri2 = URI.create("https://www.ruoxue.org?s=%61");
+		System.out.println(uri2);
+		result = uri.compareTo(uri2);
+		System.out.println(result);
+		assertThat(result).isGreaterThan(0);
+	}
+
+	@Test
+	public void isAbsolute() {
+		URI uri = URI.create("https://www.ruoxue.org");
+		System.out.println(uri);
+		boolean result = uri.isAbsolute();
+		System.out.println(result + ", getScheme: " + uri.getScheme());
+		assertThat(result).isTrue();
+
+		uri = URI.create("java-net");
+		System.out.println(uri);
+		result = uri.isAbsolute();
+		System.out.println(result + ", getScheme: " + uri.getScheme());
+		assertThat(result).isFalse();
+	}
+
+	@Test
+	public void isOpaque() {
+		URI uri = URI.create("mailto:ruoxueorg@gmail.com");
+		System.out.println(uri);
+		boolean result = uri.isOpaque();
+		System.out.println(result + ", getPath: " + uri.getPath());
+		assertThat(result).isTrue();
+
+		uri = URI.create("java-net");
+		System.out.println(uri);
+		result = uri.isOpaque();
+		System.out.println(result + ", getPath: " + uri.getPath());
+		assertThat(result).isFalse();
+	}
 }
