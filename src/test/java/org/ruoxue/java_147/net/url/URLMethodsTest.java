2a01881279ac3bfb572c70a5af9d4632edc6dbc5
ruoxue <ruoxue777@gmail.com>
2023-11-06 15:50:59 +0800

    update

--- src/test/java/org/ruoxue/java_147/net/url/URLMethodsTest.java
+++ src/test/java/org/ruoxue/java_147/net/url/URLMethodsTest.java
@@ -4,10 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
@@ -51,7 +51,7 @@ public class URLMethodsTest {
 	@Test
 	public void constructorThrowException() {
 		assertThatCode(() -> {
-			String value = "urn:isbn:1234567890+%25";
+			String value = "/java-learn/java-net";
 			URL url = new URL(value);
 			System.out.println(url);
 		}).isInstanceOf(MalformedURLException.class);
@@ -101,12 +101,33 @@ public class URLMethodsTest {
 	@Test
 	public void openStream() {
 		try {
-			String value = "https://www.ruoxue.org";
-			URL url = new URL(value);
+			URL url = new URL("https://www.ruoxue.org");
 			System.out.println(url);
-			String result = IOUtils.toString(url.openStream(), StandardCharsets.UTF_8.toString());
-			// System.out.println(result);
-			assertThat(result).hasSizeGreaterThan(0);
+			try (InputStream in = url.openStream()) {
+				String result = IOUtils.toString(in, StandardCharsets.UTF_8.toString());
+				System.out.println(result.length());
+				assertThat(result).hasSizeGreaterThan(0);
+			}
+		} catch (Exception ex) {
+			throw new RuntimeException(ex.getMessage(), ex);
+		}
+	}
+
+	@Test
+	public void getContent() {
+		try {
+			URL url = new URL("https://www.ruoxue.org");
+			System.out.println(url.getContent());
+			StringBuilder result = new StringBuilder();
+			try (BufferedReader in = new BufferedReader(new InputStreamReader((InputStream) url.getContent()))) {
+				String value;
+				while ((value = in.readLine()) != null) {
+					result.append(value);
+				}
+				System.out.println(result.length());
+				assertThat(result).hasSizeGreaterThan(0);
+			}
+
 		} catch (Exception ex) {
 			throw new RuntimeException(ex.getMessage(), ex);
 		}
