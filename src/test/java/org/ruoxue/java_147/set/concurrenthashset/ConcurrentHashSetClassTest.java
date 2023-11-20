f19952bb457269b7fb202e67a5478ba442a4bd9b
ruoxue <ruoxue777@gmail.com>
2023-11-20 17:59:46 +0800

    update

---
+++ src/test/java/org/ruoxue/java_147/set/concurrenthashset/ConcurrentHashSetClassTest.java
@@ -0,0 +1,94 @@
+package org.ruoxue.java_147.set.concurrenthashset;
+
+import static org.junit.Assert.*;
+
+import java.util.ConcurrentHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+public class ConcurrentHashSetClassTest {
+
+	@Test
+	public void contains() {
+		Set<String> set = new ConcurrentHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+
+		boolean contains = set.contains("Longan");
+		System.out.println(contains);
+		assertTrue(contains);
+
+		contains = set.contains("Grape");
+		System.out.println(contains);
+		assertFalse(contains);
+	}
+
+	@Test
+	public void containsAll() {
+		Set<String> set = new ConcurrentHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+
+		Set<String> set2 = new ConcurrentHashSet<String>();
+		set2.add("Longan");
+		set2.add("Tomato");
+
+		boolean contains = set.containsAll(set2);
+		System.out.println(contains);
+		assertTrue(contains);
+
+		contains = set2.containsAll(set);
+		System.out.println(contains);
+		assertFalse(contains);
+	}
+
+	@Test
+	public void stream() {
+		int expectedSize = 1;
+		Set<String> set = new ConcurrentHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+		set = set.stream().filter(e -> e.length() < 6).collect(Collectors.toSet());
+		System.out.println(set);
+		assertEquals(expectedSize, set.size());
+	}
+
+	@Test
+	public void parallelStream() {
+		Set<String> set = new ConcurrentHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+		set.parallelStream().forEach(System.out::println);
+		System.out.println("----------");
+		set.parallelStream().forEachOrdered(System.out::println);
+	}
+
+	@Test
+	public void retainAll() {
+		Set<String> set = new ConcurrentHashSet<String>();
+		set.add("Longan");
+		set.add("Tomato");
+		set.add("Pear");
+
+		Set<String> set2 = new ConcurrentHashSet<String>();
+		set2.add("Longan");
+		set2.add("Lemon");
+		set2.add("Mango");
+
+		boolean result = set.retainAll(set2);
+		System.out.println(result);
+		System.out.println(set);
+		assertTrue(result);
+
+		result = set.retainAll(set2);
+		System.out.println(result);
+		System.out.println(set);
+		assertFalse(result);
+	}
+}
---
+++ src/test/java/org/ruoxue/java_147/set/concurrenthashset/README.md
@@ -0,0 +1,6 @@
+# Java 147 ConcurrentHashSet
+
+## Related Articles
+1. [Java ConcurrentHashSet Methods](https://www.ruoxue.org/java-147-java-concurrenthashset-methods/)
+2. [ConcurrentHashSet in Java with Examples](https://www.ruoxue.org/java-147-concurrenthashset-in-java-with-examples/)
+3. [Java ConcurrentHashSet Class](https://www.ruoxue.org/java-147-java-concurrenthashset-class/)
