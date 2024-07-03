53002e850327e68491c19dd0f2ab559c94d9a2ac
ruoxue <ruoxue777@gmail.com>
2024-07-03 10:41:49 +0800

    update

--- src/test/java/org/ruoxue/java_147/net/DifferenceUriUrlTest.java
+++ src/test/java/org/ruoxue/java_147/net/DifferenceUriUrlTest.java
@@ -2,9 +2,14 @@ package org.ruoxue.java_147.net;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 public class DifferenceUriUrlTest {
@@ -33,40 +38,54 @@ public class DifferenceUriUrlTest {
 		assertThat(uri.getQuery()).isEqualTo("amount=101");
 		assertThat(uri.getFragment()).isEqualTo("top");
 	}
-	
-	@Test
-	public void createURL() {
-		try {
-			String value = "http://www.ruoxue.org/java-learn";
-			URL url = new URL(value);
-			System.out.println(url);
-			System.out.println("Protocol: " + url.getProtocol());
-			System.out.println("Authority: " + url.getAuthority());
-			System.out.println("UserInfo: " + url.getUserInfo());
-			System.out.println("Host: " + url.getHost());
-			System.out.println("Port: " + url.getPort());
-			System.out.println("DefaultPort: " + url.getDefaultPort());
-			System.out.println("File: " + url.getFile());
-			System.out.println("Path: " + url.getPath());
-			System.out.println("Query: " + url.getQuery());
-			System.out.println("Ref: " + url.getRef());
 
-			URL url2 = new URL("http", "www.ruoxue.org", 80, "/java-learn");
-			System.out.println(url2);
-			assertThat(url.getProtocol()).isEqualTo(url2.getProtocol());
-			assertThat(url.getHost()).isEqualTo(url2.getHost());
-			assertThat(url.getDefaultPort()).isEqualTo(url2.getDefaultPort());
-			assertThat(url.getFile()).isEqualTo(url2.getFile());
-			assertThat(url.getPath()).isEqualTo(url2.getPath());
-			assertThat(url.getQuery()).isEqualTo(url2.getQuery());
-			assertThat(url.getRef()).isEqualTo(url2.getRef());
-		} catch (MalformedURLException ex) {
-			throw new RuntimeException(ex.getMessage(), ex);
-		}		
-	}
-	
 	@Test
-	public void conversion() {
-		
+	public void createURL() throws MalformedURLException {
+		String value = "http://www.ruoxue.org/java-learn";
+		URL url = new URL(value);
+		System.out.println(url);
+		System.out.println("Protocol: " + url.getProtocol());
+		System.out.println("Authority: " + url.getAuthority());
+		System.out.println("UserInfo: " + url.getUserInfo());
+		System.out.println("Host: " + url.getHost());
+		System.out.println("Port: " + url.getPort());
+		System.out.println("DefaultPort: " + url.getDefaultPort());
+		System.out.println("File: " + url.getFile());
+		System.out.println("Path: " + url.getPath());
+		System.out.println("Query: " + url.getQuery());
+		System.out.println("Ref: " + url.getRef());
+
+		URL url2 = new URL("http", "www.ruoxue.org", 80, "/java-learn");
+		System.out.println(url2);
+		assertThat(url.getProtocol()).isEqualTo(url2.getProtocol());
+		assertThat(url.getHost()).isEqualTo(url2.getHost());
+		assertThat(url.getDefaultPort()).isEqualTo(url2.getDefaultPort());
+		assertThat(url.getFile()).isEqualTo(url2.getFile());
+		assertThat(url.getPath()).isEqualTo(url2.getPath());
+		assertThat(url.getQuery()).isEqualTo(url2.getQuery());
+		assertThat(url.getRef()).isEqualTo(url2.getRef());
+	}
+
+	@Test
+	public void conversion() throws URISyntaxException, MalformedURLException {
+		String value = "https://host:443/path?query";
+		URI uri = new URI(value);
+		URL url = new URL(value);
+
+		URL toURL = uri.toURL();
+		URI toURI = url.toURI();
+
+		assertThat(url).isNotNull();
+		assertThat(uri).isNotNull();
+		System.out.println("toURL: " + toURL.toString());
+		System.out.println("toURI: " + toURI.toString());
+		assertThat(toURL.toString()).isEqualTo(toURI.toString());
+	}
+
+	@Test
+	public void openStream() throws IOException {
+		URL url = new URL("https://www.ruoxue.org");
+		String contents = IOUtils.toString(url.openStream(), StandardCharsets.UTF_8);
+		assertThat(contents).isNotNull();
 	}
 }
